tudelft/paparazzi

commit b80cc08bc7ce6fbc4d399f21c7a95adb40d834e3

[gps] update gps_mtk to use generic device

sw/airborne/subsystems/gps/gps_mtk.c
@@ -393,9 +393,12 @@ restart:
  */
 #ifdef GPS_CONFIGURE
 
+#include "mcu_periph/link_device.h"
+
 static void MtkSend_CFG(char *dat)
 {
-  while (*dat != 0) { GpsLink(Transmit(*dat++)); }
+  struct link_device *dev = &((GPS_LINK).device);
+  while (*dat != 0) { dev->transmit(dev->periph, *dat++); }
 }
 
 void gps_configure_uart(void)

sw/airborne/subsystems/gps/gps_mtk.h
@@ -58,7 +58,6 @@ extern struct GpsMtk gps_mtk;
 #define _GpsLink(dev, _x)  __GpsLink(dev, _x)
 #define GpsLink(_x) _GpsLink(GPS_LINK, _x)
 
-#define GpsBuffer() GpsLink(ChAvailable())
 
 #ifdef GPS_CONFIGURE
 extern bool_t gps_configuring;
@@ -70,43 +69,42 @@ extern bool_t gps_configuring;
 #define GpsConfigure() {}
 #endif
 
-#define GpsEvent(_sol_available_callback) {             \
-    if (GpsBuffer()) {                                  \
-      ReadGpsBuffer();                                  \
-      GpsConfigure();                                   \
-    }                                                   \
-    if (gps_mtk.msg_available) {                        \
-      gps.last_msg_ticks = sys_time.nb_sec_rem;         \
-      gps.last_msg_time = sys_time.nb_sec;              \
-      gps_mtk_read_message();                           \
-      if (gps_mtk.msg_class == MTK_DIY14_ID &&          \
-          gps_mtk.msg_id == MTK_DIY14_NAV_ID) {         \
-        if (gps.fix == GPS_FIX_3D) {                    \
-          gps.last_3dfix_ticks = sys_time.nb_sec_rem;   \
-          gps.last_3dfix_time = sys_time.nb_sec;        \
-        }                                               \
-        _sol_available_callback();                      \
-      }                                                 \
-      if (gps_mtk.msg_class == MTK_DIY16_ID &&          \
-          gps_mtk.msg_id == MTK_DIY16_NAV_ID) {         \
-        if (gps.fix == GPS_FIX_3D) {                    \
-          gps.last_3dfix_ticks = sys_time.nb_sec_rem;   \
-          gps.last_3dfix_time = sys_time.nb_sec;        \
-        }                                               \
-        _sol_available_callback();                      \
-      }                                                 \
-      gps_mtk.msg_available = FALSE;                    \
-    }                                                   \
-  }
-
-#define ReadGpsBuffer() {         \
-    while (GpsLink(ChAvailable())&&!gps_mtk.msg_available)  \
-      gps_mtk_parse(GpsLink(Getch()));      \
-  }
-
-
 extern void gps_mtk_read_message(void);
 extern void gps_mtk_parse(uint8_t c);
+
+static inline void GpsEvent(void (* _sol_available_callback)(void))
+{
+  struct link_device *dev = &((GPS_LINK).device);
+
+  if (dev->char_available(dev->periph)) {
+    while (dev->char_available(dev->periph) && !gps_ubx.msg_available) {
+      gps_mtk_parse(dev->getchar(dev->periph));
+    }
+    GpsConfigure();
+  }
+  if (gps_mtk.msg_available) {
+    gps.last_msg_ticks = sys_time.nb_sec_rem;
+    gps.last_msg_time = sys_time.nb_sec;
+    gps_mtk_read_message();
+    if (gps_mtk.msg_class == MTK_DIY14_ID &&
+        gps_mtk.msg_id == MTK_DIY14_NAV_ID) {
+      if (gps.fix == GPS_FIX_3D) {
+        gps.last_3dfix_ticks = sys_time.nb_sec_rem;
+        gps.last_3dfix_time = sys_time.nb_sec;
+      }
+      _sol_available_callback();
+    }
+    if (gps_mtk.msg_class == MTK_DIY16_ID &&
+        gps_mtk.msg_id == MTK_DIY16_NAV_ID) {
+      if (gps.fix == GPS_FIX_3D) {
+        gps.last_3dfix_ticks = sys_time.nb_sec_rem;
+        gps.last_3dfix_time = sys_time.nb_sec;
+      }
+      _sol_available_callback();
+    }
+    gps_mtk.msg_available = FALSE;
+  }
+}
 
 /*
  * dynamic GPS configuration

sw/tools/generators/gen_mtk.ml
@@ -105,33 +105,7 @@ let parse_message = fun class_name m ->
       if l <> !offset then raise (Length_error (m, l, !offset))
     with
         Xml.No_attribute("length") -> () (** Undefined length authorized *)
-  end;
-
-  (** Generating send function *)
-  let param_name = fun f -> String.lowercase (field_name f) in
-  let rec param_names = fun f r ->
-    if Xml.tag f = "field" then
-      param_name f :: r
-    else
-      List.fold_right param_names (Xml.children f) r in
-  let param_type = fun f -> c_type (format f) in
-  fprintf out "\n#define MtkSend_%s_%s(" class_name msg_name;
-  fprintf out "%s" (String.concat "," (param_names m []));
-  fprintf out ") { \\\n";
-  fprintf out "  MtkHeader(MTK_%s_ID, %s, %d);\\\n" class_name msg_id !offset;
-  let rec send_one_field = fun f ->
-    match Xml.tag f with
-        "field" ->
-          let s = sizeof (format f) in
-          let p = param_name f in
-          let t = param_type f in
-          fprintf out "  %s _%s = %s; MtkSend%dByAddr((uint8_t*)&_%s);\\\n" t p p s p
-      | "block" ->
-        List.iter send_one_field (Xml.children f)
-      | _ -> assert (false) in
-  List.iter send_one_field (Xml.children m);
-  fprintf out "  MtkTrailer();\\\n";
-  fprintf out "}\n\n#define MTK_%s_%s_LENGTH %d\n" class_name msg_name !offset
+  end
 
 
 let parse_class = fun c ->

commit 45662920e81e3733ed8e1d0b9b12b54f9d863384

[gps] fix gps mtk compilation
this has been broken since b80cc08bc7ce6fbc4d399f21c7a95adb40d834e3 and is probably not tested at all...

sw/airborne/subsystems/gps/gps_mtk.c
@@ -19,7 +19,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/** @file gps_mtk.h
+/** @file gps_mtk.c
  * @brief Mediatek MT3329 specific code
  *
  * supports:
@@ -139,7 +139,7 @@ void gps_mtk_msg(void)
 }
 
 static void gps_mtk_time2itow(uint32_t  gps_date, uint32_t  gps_time,
-                              int16_t *gps_week, uint32_t *gps_itow)
+                              uint16_t *gps_week, uint32_t *gps_itow)
 {
   /* convert UTC date/time to GPS week/itow, we have no idea about GPS
      leap seconds for now */

sw/airborne/subsystems/gps/gps_mtk.h
@@ -19,6 +19,17 @@
  * Boston, MA 02111-1307, USA.
  */
 
+/** @file gps_mtk.h
+ * @brief Mediatek MT3329 specific code
+ *
+ * supports:
+ *   DIYDrones V1.4 protocol (AXN1.30_2278)
+ *   DIYDrones V1.6 protocol (AXN1.30_2389)
+ *
+ * documentation is partly incorrect, see mtk.xml for what seems
+ * to be "real"
+ *
+ */
 
 #ifndef MTK_H
 #define MTK_H
@@ -57,6 +68,8 @@ extern struct GpsMtk gps_mtk;
 #include "pprzlink/pprzlink_device.h"
 
 #ifdef GPS_CONFIGURE
+extern void gps_configure(void);
+extern void gps_configure_uart(void);
 extern bool_t gps_configuring;
 #define GpsConfigure() {            \
     if (gps_configuring)            \
@@ -68,6 +81,7 @@ extern bool_t gps_configuring;
 
 extern void gps_mtk_read_message(void);
 extern void gps_mtk_parse(uint8_t c);
+extern void gps_mtk_msg(void);
 
 static inline void GpsEvent(void)
 {
@@ -81,13 +95,5 @@ static inline void GpsEvent(void)
     GpsConfigure();
   }
 }
-
-/*
- * dynamic GPS configuration
- */
-#ifdef GPS_CONFIGURE
-extern void gps_configure(void);
-extern void gps_configure_uart(void);
-#endif
 
 #endif /* MTK_H */

sw/tools/generators/gen_mtk.ml
@@ -105,7 +105,8 @@ let parse_message = fun class_name m ->
       if l <> !offset then raise (Length_error (m, l, !offset))
     with
         Xml.No_attribute("length") -> () (** Undefined length authorized *)
-  end
+  end;
+  fprintf out "\n#define MTK_%s_%s_LENGTH %d\n" class_name msg_name !offset
 
 
 let parse_class = fun c ->