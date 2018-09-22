8bfb6dd1bc373d2e4b460549557f7617b3b10830
Freek van Tienen <freek.v.tienen@gmail.com>
2018-09-22 06:51:42 -0700

    Fix aircraft

--- sw/ground_segment/python/mission/aircraft.py
+++ sw/ground_segment/python/mission/aircraft.py
@@ -50,6 +50,10 @@ class Aircraft(object):
         self.ac_id = ac_id
         self.got_ltp = False
         self.ltp_def = None
+        self.lla = geodetic.LlaCoor_f(0.0, 0.0, 0.0)
+        self.course = 0.0
+        self.ground_speed = 0.0
+        self.airspeed = 0.0
         self.lock = threading.Lock()
 
         # Create a new ivy interface if needed
@@ -60,6 +64,7 @@ class Aircraft(object):
 
         self.ivy_interface.subscribe(self.aircraft_ins_ref_cb, PprzMessage("telemetry", "INS_REF"))
         self.ivy_interface.subscribe(self.aircraft_navigation_ref_cb, PprzMessage("telemetry", "NAVIGATION_REF"))
+        self.ivy_interface.subscribe(self.ground_flight_param_cb, PprzMessage("ground", "FLIGHT_PARAM"))
 
     def aircraft_ins_ref_cb(self, ac_id, msg):
         """
@@ -92,6 +97,28 @@ class Aircraft(object):
             self.ltp_def = geodetic.UtmCoor_i(int(msg['utm_north'])*100, int(msg['utm_east'])*100, int(float(msg['ground_alt'])*1000), int(msg['utm_zone'])).to_float().to_lla().to_ltp_def()
             self.got_ltp = True
             logging.debug("Got LTP from aircraft")
+        finally:
+            self.lock.release()
+
+    def ground_flight_param_cb(self, ac_id, msg):
+        """
+        When a INS_REF message is received from the AC
+        """
+        # Check if it is for our aircraft
+        if self.ac_id != int(msg['ac_id']):
+            return
+        
+        try:
+            self.lock.acquire()
+            
+            self.lla = geodetic.LlaCoor_f(float(msg['lat']), float(msg['long']), float(msg['alt']))
+            self.course = float(msg['course'])
+            self.ground_speed = float(msg['speed'])
+            self.airspeed = float(msg['airspeed'])
+            
+            logging.debug("Got the Flight parameters [%f,%f,%f, %f, %f, %f]",
+                float(msg['lat']), float(msg['long']), float(msg['alt']),
+                float(msg['course']), float(msg['speed']), float(msg['airspeed']))
         finally:
             self.lock.release()
 
