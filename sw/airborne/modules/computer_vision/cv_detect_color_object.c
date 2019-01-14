0392dfe1ca0cf7d721675f79209d74ce1d2fc878
kirkscheper <kirkscheper@gmail.com>
2019-01-14 22:06:53 +0100

    updated orange avoider to module state machine and added simple guided mode example

---
+++ sw/airborne/modules/computer_vision/cv_detect_color_object.c
@@ -0,0 +1,230 @@
+/*
+ * Copyright (C) 2019 Kirk Scheper <kirkscheper@gmail.com>
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * @file modules/computer_vision/cv_detect_object.h
+ * Assumes the object consists of a continuous color and checks
+ * if you are over the defined object or not
+ */
+
+// Own header
+#include "modules/computer_vision/cv_detect_color_object.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "modules/computer_vision/cv.h"
+#include "subsystems/abi.h"
+#include "std.h"
+
+#define PRINT(string,...) fprintf(stderr, "[object_detector->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
+#if OBJECT_DETECTOR_VERBOSE
+#define VERBOSE_PRINT PRINT
+#else
+#define VERBOSE_PRINT(...)
+#endif
+
+#ifndef COLOR_OBJECT_DETECTOR_FPS1
+#define COLOR_OBJECT_DETECTOR_FPS1 0 ///< Default FPS (zero means run at camera fps)
+#endif
+#ifndef COLOR_OBJECT_DETECTOR_FPS2
+#define COLOR_OBJECT_DETECTOR_FPS2 0 ///< Default FPS (zero means run at camera fps)
+#endif
+
+// Filter Settings
+uint8_t cod_lum_min1 = 0;
+uint8_t cod_lum_max1 = 0;
+uint8_t cod_cb_min1 = 0;
+uint8_t cod_cb_max1 = 0;
+uint8_t cod_cr_min1 = 0;
+uint8_t cod_cr_max1 = 0;
+
+uint8_t cod_lum_min2 = 0;
+uint8_t cod_lum_max2 = 0;
+uint8_t cod_cb_min2 = 0;
+uint8_t cod_cb_max2 = 0;
+uint8_t cod_cr_min2 = 0;
+uint8_t cod_cr_max2 = 0;
+
+bool cod_draw1 = false;
+bool cod_draw2 = false;
+
+// Function
+uint32_t find_object_centroid(struct image_t *img, int32_t* p_xc, int32_t* p_yc, bool draw,
+                              uint8_t lum_min, uint8_t lum_max,
+                              uint8_t cb_min, uint8_t cb_max,
+                              uint8_t cr_min, uint8_t cr_max);
+
+/*
+ * object_detector
+ * @param img - input image to process
+ * @param filter - which detection filter to process
+ * @return img
+ */
+static struct image_t *object_detector(struct image_t *img, uint8_t filter)
+{
+  uint8_t lum_min, lum_max;
+  uint8_t cb_min, cb_max;
+  uint8_t cr_min, cr_max;
+  bool draw;
+  uint8_t obstacle_id;
+
+  switch (filter){
+    case 1:
+      lum_min = cod_lum_min1;
+      lum_max = cod_lum_max1;
+      cb_min = cod_cb_min1;
+      cb_max = cod_cb_max1;
+      cr_min = cod_cr_min1;
+      cr_max = cod_cr_max1;
+      draw = cod_draw1;
+      obstacle_id = COLOR_OBJECT_DETECTION1_ID;
+      break;
+    case 2:
+      lum_min = cod_lum_min2;
+      lum_max = cod_lum_max2;
+      cb_min = cod_cb_min2;
+      cb_max = cod_cb_max2;
+      cr_min = cod_cr_min2;
+      cr_max = cod_cr_max2;
+      draw = cod_draw2;
+      obstacle_id = COLOR_OBJECT_DETECTION2_ID;
+      break;
+    default:
+      return img;
+  };
+
+  int32_t x_c, y_c;
+
+  // Filter and find centroid
+  uint32_t count = find_object_centroid(img, &x_c, &y_c, draw, lum_min, lum_max, cb_min, cb_max, cr_min, cr_max);
+  VERBOSE_PRINT("Color count %d: %u, threshold %u, x_c %d, y_c %d\n", camera, object_count, count_threshold, x_c, y_c);
+  AbiSendMsgVISUAL_DETECTION(obstacle_id, x_c, y_c, 0, 0, count, filter);
+  VERBOSE_PRINT("centroid %d: (%d, %d) r: %4.2f a: %4.2f\n", camera, x_c, y_c,
+        hypotf(x_c, y_c) / hypotf(img->w * 0.5, img->h * 0.5), RadOfDeg(atan2f(y_c, x_c)));
+
+  return img;
+}
+
+struct image_t *object_detector1(struct image_t *img);
+struct image_t *object_detector1(struct image_t *img)
+{
+  return object_detector(img, 1);
+}
+
+struct image_t *object_detector2(struct image_t *img);
+struct image_t *object_detector2(struct image_t *img)
+{
+  return object_detector(img, 2);
+}
+
+void color_object_detector_init(void)
+{
+#ifdef COLOR_OBJECT_DETECTOR_CAMERA1
+#ifdef COLOR_OBJECT_DETECTOR_LUM_MIN1
+  cod_lum_min1 = COLOR_OBJECT_DETECTOR_LUM_MIN1;
+  cod_lum_max1 = COLOR_OBJECT_DETECTOR_LUM_MAX1;
+  cod_cb_min1 = COLOR_OBJECT_DETECTOR_CB_MIN1;
+  cod_cb_max1 = COLOR_OBJECT_DETECTOR_CB_MAX1;
+  cod_cr_min1 = COLOR_OBJECT_DETECTOR_CR_MIN1;
+  cod_cr_max1 = COLOR_OBJECT_DETECTOR_CR_MAX1;
+#endif
+#ifdef COLOR_OBJECT_DETECTOR_DRAW1
+  cod_draw1 = COLOR_OBJECT_DETECTOR_DRAW1;
+#endif
+
+  cv_add_to_device(&COLOR_OBJECT_DETECTOR_CAMERA1, object_detector1, COLOR_OBJECT_DETECTOR_FPS1);
+#endif
+
+#ifdef COLOR_OBJECT_DETECTOR_CAMERA2
+#ifdef COLOR_OBJECT_DETECTOR_LUM_MIN2
+  cod_lum_min2 = COLOR_OBJECT_DETECTOR_LUM_MIN2;
+  cod_lum_max2 = COLOR_OBJECT_DETECTOR_LUM_MAX2;
+  cod_cb_min2 = COLOR_OBJECT_DETECTOR_CB_MIN2;
+  cod_cb_max2 = COLOR_OBJECT_DETECTOR_CB_MAX2;
+  cod_cr_min2 = COLOR_OBJECT_DETECTOR_CR_MIN2;
+  cod_cr_max2 = COLOR_OBJECT_DETECTOR_CR_MAX2;
+#endif
+#ifdef COLOR_OBJECT_DETECTOR_DRAW2
+  cod_draw2 = COLOR_OBJECT_DETECTOR_DRAW2;
+#endif
+
+  cv_add_to_device(&COLOR_OBJECT_DETECTOR_CAMERA2, object_detector2, COLOR_OBJECT_DETECTOR_FPS2);
+#endif
+}
+
+/*
+ * find_object_centroid
+ * @param img - input image to process
+ * @param p_xc - x coordinate of the centroid of color object
+ * @param p_yc - y coordinate of the centroid of color object
+ * @param draw - whether or not to draw on image
+ * @return number of pixels found
+ */
+uint32_t find_object_centroid(struct image_t *img, int32_t* p_xc, int32_t* p_yc, bool draw,
+                              uint8_t lum_min, uint8_t lum_max,
+                              uint8_t cb_min, uint8_t cb_max,
+                              uint8_t cr_min, uint8_t cr_max)
+{
+  uint32_t cnt = 0;
+  uint32_t tot_x = 0;
+  uint32_t tot_y = 0;
+  uint8_t *buffer = img->buf;
+
+  // Go through all the pixels
+  for (uint16_t y = 0; y < img->h; y++) {
+    for (uint16_t x = 0; x < img->w; x ++) {
+      // Check if the color is inside the specified values
+      uint8_t *yp, *up, *vp;
+      if (x % 2 == 0) {
+        // Even x
+        up = &buffer[y * 2 * img->w + 2 * x];      // U
+        yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y1
+        vp = &buffer[y * 2 * img->w + 2 * x + 2];  // V
+        //yp = &buffer[y * 2 * img->w + 2 * x + 3]; // Y2
+      } else {
+        // Uneven x
+        up = &buffer[y * 2 * img->w + 2 * x - 2];  // U
+        //yp = &buffer[y * 2 * img->w + 2 * x - 1]; // Y1
+        vp = &buffer[y * 2 * img->w + 2 * x];     // V
+        yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y2
+      }
+      if ( (*yp >= lum_min) && (*yp <= lum_max) &&
+           (*up >= cb_min ) && (*up <= cb_max ) &&
+           (*vp >= cr_min ) && (*vp <= cr_max )) {
+        cnt ++;
+        tot_x += x;
+        tot_y += y;
+        if (draw){
+          *yp = 255;  // make pixel brighter in image
+        }
+      }
+    }
+  }
+  if (cnt > 0) {
+    *p_xc = (int32_t) roundf(tot_x / ((float) cnt) - img->w * 0.5f);
+    *p_yc = (int32_t) roundf(img->h * 0.5f - tot_y / ((float) cnt));
+  } else {
+    *p_xc = 0;
+    *p_yc = 0;
+  }
+  return cnt;
+}
