6c6af4d766ccaf6adfebfe2228f1cb69add772b8
nilay994 <nilay_994@hotmail.com>
2019-04-12 11:37:18 +0200

    fix dir

--- sw/airborne/modules/ctrl/ctrl_module_outerloop_demo.c
+++ sw/airborne/modules/ctrl/ctrl_module_outerloop_demo.c
@@ -29,14 +29,18 @@
 #include "subsystems/radio_control.h"
 #include "firmwares/rotorcraft/stabilization.h"
 #include "firmwares/rotorcraft/stabilization/stabilization_attitude.h"
-#include "firmwares/rotorcraft/stabilization/stabilization_attitude_rc_setpoint.h"
-#include "autopilot.h"
+
 
 // Own Variables
 
 struct ctrl_module_demo_struct {
 // RC Inputs
   struct Int32Eulers rc_sp;
+
+  float phi;
+  float theta;
+  float psi;
+  float alt;
 
 // Output command
   struct Int32Eulers cmd;
@@ -46,6 +50,8 @@ struct ctrl_module_demo_struct {
 
 // Settings
 float comode_time = 0;
+
+#include "./dronerace/dronerace.h"
 
 
 ////////////////////////////////////////////////////////////////////
@@ -61,12 +67,14 @@ void guidance_h_module_enter(void)
   ctrl.cmd.psi = stateGetNedToBodyEulers_i()->psi;
 
   // Convert RC to setpoint
-  stabilization_attitude_read_rc_setpoint_eulers(&ctrl.rc_sp, autopilot.in_flight, false, false);
+  //stabilization_attitude_read_rc_setpoint_eulers(&rc_sp, in_flight, false, false);
+
+  dronerace_enter();
 }
 
 void guidance_h_module_read_rc(void)
 {
-  stabilization_attitude_read_rc_setpoint_eulers(&ctrl.rc_sp, autopilot.in_flight, false, false);
+  //stabilization_attitude_read_rc_setpoint_eulers(&rc_sp, in_flight, false, false);
 }
 
 
@@ -74,11 +82,11 @@ void guidance_h_module_run(bool in_flight)
 {
   // YOUR NEW HORIZONTAL OUTERLOOP CONTROLLER GOES HERE
   // ctrl.cmd = CallMyNewHorizontalOuterloopControl(ctrl);
-  float roll = 0.0;
-  float pitch = 0.0;
+  dronerace_get_cmd(&ctrl.alt, &ctrl.phi, &ctrl.theta, &ctrl.psi);
 
-  ctrl.cmd.phi = ANGLE_BFP_OF_REAL(roll);
-  ctrl.cmd.theta = ANGLE_BFP_OF_REAL(pitch);
+  ctrl.cmd.phi = ANGLE_BFP_OF_REAL(ctrl.phi);
+  ctrl.cmd.theta = ANGLE_BFP_OF_REAL(ctrl.theta);
+  ctrl.cmd.psi = ANGLE_BFP_OF_REAL(ctrl.psi);
 
   stabilization_attitude_set_rpy_setpoint_i(&(ctrl.cmd));
   stabilization_attitude_run(in_flight);
