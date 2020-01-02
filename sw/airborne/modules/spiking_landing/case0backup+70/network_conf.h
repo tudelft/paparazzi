331df87110f4bf4b91913453db470682e24ed161
Huizerd <jessehagenaars@gmail.com>
2020-01-02 17:27:38 +0100

    Only relevant cases

--- sw/airborne/modules/spiking_landing/case0backup+70/network_conf.h
+++
@@ -1,8 +0,0 @@
-//Auto-generated
-#include "Network.h"
-#include "connection_conf_inhid.h"
-#include "connection_conf_hidout.h"
-#include "neuron_conf_hid.h"
-#include "neuron_conf_out.h"
-float const centers[] = {-10.0, -5.119999885559082, -2.1600000858306885, -0.6399999856948853, -0.07999999821186066, 0.0, 0.07999999821186066, 0.6399999856948853, 2.1600000858306885, 5.119999885559082, 10.0};
-NetworkConf const conf = {0, 1.0, centers, 2, 4, 20, 1, &conf_inhid, &conf_hid, &conf_hidout, &conf_out};
