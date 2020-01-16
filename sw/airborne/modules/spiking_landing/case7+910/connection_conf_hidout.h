3ca9c345fd17878e6ae434a48f2fb15fac6d59d2
Huizerd <jessehagenaars@gmail.com>
2020-01-16 17:51:31 +0100

    One hidden neuron

---
+++ sw/airborne/modules/spiking_landing/case7+910/connection_conf_hidout.h
@@ -0,0 +1,4 @@
+//Auto-generated
+#include "Connection.h"
+float const w_hidout[] = {1.753333330154419};
+ConnectionConf const conf_hidout = {1, 1, w_hidout};
---
+++ sw/airborne/modules/spiking_landing/case7+910/neuron_conf_out.h
@@ -0,0 +1,10 @@
+//Auto-generated
+#include "Neuron.h"
+float const a_v_out[] = {0.8692938089370728};
+float const a_th_out[] = {0.0};
+float const a_t_out[] = {0.8208926916122437};
+float const d_v_out[] = {0.30000001192092896};
+float const d_th_out[] = {0.0};
+float const d_t_out[] = {0.8821069002151489};
+float const th_rest_out[] = {0.25418126583099365};
+NeuronConf const conf_out = {0, 1, a_v_out, a_th_out, a_t_out, d_v_out, d_th_out, d_t_out, 0.0, th_rest_out};
