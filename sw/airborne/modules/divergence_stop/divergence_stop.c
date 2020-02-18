68d538039faaf7cce96f460c1023b5ebedcb8b7c
matteobarbera <matteo.barbera97@gmail.com>
2020-02-18 14:29:31 +0100

    divergence_stop compiles and runs, with stabilization issues

--- sw/airborne/modules/divergence_stop/divergence_stop.c
+++ sw/airborne/modules/divergence_stop/divergence_stop.c
@@ -1,4 +1,4 @@
-#include "modules/orange_avoider/divergence_stop.h"
+#include "modules/divergence_stop/divergence_stop.h"
 #include "firmwares/rotorcraft/guidance/guidance_h.h"
 #include "generated/airframe.h"
 #include "state.h"
@@ -11,13 +11,17 @@
 #endif
 float size_divergence;
 
+#ifndef OFH_OPTICAL_FLOW_ID
+#define OFH_OPTICAL_FLOW_ID ABI_BROADCAST
+#endif
+
 #define PRINT(string,...) fprintf(stderr, "[divergence_stop->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
 
 enum navigation_state_t {
   FORWARD,
   STOP
 };
-navigation_state_t navigation_state = FORWARD
+enum navigation_state_t navigation_state = FORWARD;
 
 
 static abi_event optical_flow_ev;
@@ -30,24 +34,24 @@ static void optical_flow_cb(uint8_t sender_id __attribute__((unused)), uint32_t 
 void divergence_stop_init(void)
 {
   // Initialize by flying forward
-  navigation_state = FORWARD
+  navigation_state = FORWARD;
 
   // Subscribe to the optical flow estimator:
   AbiBindMsgOPTICAL_FLOW(OFH_OPTICAL_FLOW_ID, &optical_flow_ev, optical_flow_cb);
 
 }
 
-void orange_avoider_guided_periodic(void)
+void divergence_stop_periodic(void)
 {
   if (size_divergence > DIVERGENCE_STOP_THRESHOLD) {
-	  navigation_state = STOP
+	  navigation_state = STOP;
   }
   if (navigation_state == STOP) {
 	  guidance_h_set_guided_body_vel(0, 0);
-	  PRINT("THRESHOLD EXCEEDED")
+	  PRINT("THRESHOLD EXCEEDED\n");
   } else {
 	  guidance_h_set_guided_body_vel(1.0, 0);
   }
-  PRINT("%f", size_divergence)
+  PRINT("%f\n", size_divergence);
   return;
 }
---
+++ sw/airborne/modules/divergence_stop/divergence_stop.h
@@ -0,0 +1,7 @@
+#ifndef DIVERGENCE_STOP_H
+#define DIVERGENCE_STOP_H
+
+extern void divergence_stop_init(void);
+extern void divergence_stop_periodic(void);
+
+#endif
