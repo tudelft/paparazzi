4a7dd9c77681683c52d29726cf5c6e15829e7407
matteobarbera <matteo.barbera97@gmail.com>
2020-05-27 14:42:27 +0200

    Added a simple feed forward module with GCS interface

--- sw/airborne/modules/fs_landing/feed_forward.c
+++ sw/airborne/modules/fs_landing/feed_forward.c
@@ -4,12 +4,43 @@
 
 #include "feed_forward.h"
 
+float ff_duration = 3.;
+float thr_l_value = 0;
+float thr_r_value = 0;
+float elev_l_value = 0;
+float elev_r_value = 0;
+
 void ff_actuator_values(struct fs_landing_t *actuator_values, float ff_start_time, uint8_t *has_ff_finished)
 {
-    if (get_sys_time_float() - ff_start_time < FF_DURATION)
+    if (get_sys_time_float() - ff_start_time < ff_duration)
     {
-
+        actuator_values->commands[SERVO_S_THROTTLE_LEFT] = thr_l_value;
+        actuator_values->commands[SERVO_S_THROTTLE_RIGHT] = thr_r_value;
+        actuator_values->commands[SERVO_S_ELEVON_LEFT] = elev_l_value;
+        actuator_values->commands[SERVO_S_ELEVON_RIGHT] = elev_r_value;
     } else {
         has_ff_finished = true;
     }
 }
+
+void feed_forward_ff_duration_handler(float dur)
+{
+    ff_duration = dur;
+}
+void feed_forward_thr_l_handler(float val)
+{
+    thr_l_value = val;
+}
+void feed_forward_thr_r_handler(float val)
+{
+    thr_r_value = val;
+}
+void feed_forward_elev_l_handler(float val)
+{
+    elev_l_value = val;
+}
+void feed_forward_elev_r_handler(float val)
+{
+    elev_r_value = val;
+}
+
--- sw/airborne/modules/fs_landing/feed_forward.h
+++ sw/airborne/modules/fs_landing/feed_forward.h
@@ -8,8 +8,19 @@
 #include "mcu_periph/sys_time.h"
 #include "fs_landing.h"
 
-#define FF_DURATION 5
+extern float ff_duration;
+extern float thr_l_value;
+extern float thr_r_value;
+extern float elev_l_value;
+extern float elev_r_value;
 
 void ff_actuator_values(struct fs_landing_t *actuator_values, float ff_start_time, uint8_t *has_ff_finished);
+
+// Handlers for changing gcs variables
+extern void feed_forward_ff_duration_handler(float dur);
+extern void feed_forward_thr_l_handler(float val);
+extern void feed_forward_thr_r_handler(float val);
+extern void feed_forward_elev_l_handler(float val);
+extern void feed_forward_elev_r_handler(float val);
 
 #endif //PAPARAZZI_FEED_FORWARD_H
