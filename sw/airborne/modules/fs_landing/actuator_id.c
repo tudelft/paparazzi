fc91cdaf390371970d441424a118351fc8e423ca
matteobarbera <matteo.barbera97@gmail.com>
2020-06-23 09:40:04 +0200

    added option vary both motors at once in actuator_id

--- sw/airborne/modules/fs_landing/actuator_id.c
+++ sw/airborne/modules/fs_landing/actuator_id.c
@@ -8,6 +8,12 @@ uint8_t selected_actuator = 1;
 float max_delta_amplitude = 0.f;
 
 void add_chirp(struct fs_landing_t *actuator_values) {
-    float current_yaw = stateGetNedToBodyEulers_f()->psi;
-    actuator_values->commands[selected_actuator] += (int32_t) (max_delta_amplitude * sinf(current_yaw));
+    float current_yaw = stateGetNedToBodyEulers_f() -> psi;
+    if (selected_actuator == 4) {
+        // Assuming CCW spin
+        actuator_values -> commands[SERVO_S_THROTTLE_LEFT] -= (int32_t) (max_delta_amplitude * sinf(current_yaw));
+        actuator_values -> commands[SERVO_S_THROTTLE_RIGHT] += (int32_t) (max_delta_amplitude * sinf(current_yaw));
+    } else {
+        actuator_values -> commands[selected_actuator] += (int32_t)(max_delta_amplitude * sinf(current_yaw));
+    }
 }
