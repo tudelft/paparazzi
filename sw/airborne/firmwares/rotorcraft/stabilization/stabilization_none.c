7ebb699ee5a691d97f01ed043d37b33f50497a0d
Christophe De Wagter <dewagter@gmail.com>
2020-10-22 11:28:39 +0200

    Forward Manual Rate

--- sw/airborne/firmwares/rotorcraft/stabilization/stabilization_none.c
+++ sw/airborne/firmwares/rotorcraft/stabilization/stabilization_none.c
@@ -31,6 +31,9 @@
 
 #include "subsystems/radio_control.h"
 #include "generated/airframe.h"
+#include "subsystems/imu.h"
+//#include "state.h"
+#include "modules/fbw_settings/fbw_settings.h"
 
 struct Int32Rates stabilization_none_rc_cmd;
 
@@ -42,9 +45,9 @@ void stabilization_none_init(void)
 void stabilization_none_read_rc(void)
 {
 
-  stabilization_none_rc_cmd.p = (int32_t)radio_control.values[RADIO_ROLL];
-  stabilization_none_rc_cmd.q = (int32_t)radio_control.values[RADIO_PITCH];
-  stabilization_none_rc_cmd.r = (int32_t)radio_control.values[RADIO_YAW];
+  stabilization_none_rc_cmd.p = (int32_t)radio_control.values[RADIO_YAW]/4;
+  stabilization_none_rc_cmd.q = (int32_t)radio_control.values[RADIO_PITCH]/2;
+  stabilization_none_rc_cmd.r = -(int32_t)radio_control.values[RADIO_ROLL]/4;
 }
 
 void stabilization_none_enter(void)
@@ -54,8 +57,18 @@ void stabilization_none_enter(void)
 
 void stabilization_none_run(bool in_flight __attribute__((unused)))
 {
+  //float pitchrate = stateGetBodyRates_f()->q; //RATE_FLOAT_OF_BFP(imu.gyro.p);
+  float pitchrate = RATE_FLOAT_OF_BFP(imu.gyro.q);
+  //float yawrate = stateGetBodyRates_f()->p;//RATE_FLOAT_OF_BFP(imu.gyro.r); // Radians/second
+  float yawrate = RATE_FLOAT_OF_BFP(imu.gyro.r); // Radians/second
   /* just directly pass rc commands through */
-  stabilization_cmd[COMMAND_ROLL]  = stabilization_none_rc_cmd.p;
-  stabilization_cmd[COMMAND_PITCH] = stabilization_none_rc_cmd.q;
+
+  int32_t pitchdamper =  (int32_t) (pitchrate * fbw_setting_damper_pitch);
+  Bound(pitchdamper , -3000, 3000);
+  int32_t yawdamper =  (int32_t) (yawrate * fbw_setting_damper_yaw);
+  Bound(yawdamper , -3000, 3000);
+
+  stabilization_cmd[COMMAND_ROLL]  = stabilization_none_rc_cmd.p + yawdamper;
+  stabilization_cmd[COMMAND_PITCH] = stabilization_none_rc_cmd.q + pitchdamper;
   stabilization_cmd[COMMAND_YAW]   = stabilization_none_rc_cmd.r;
 }
---
+++ sw/airborne/modules/fbw_settings/fbw_settings.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) MAVLAB
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/fbw_settings/fbw_settings.c"
+ * @author MAVLAB
+ * FBW settings
+ */
+
+#include "modules/fbw_settings/fbw_settings.h"
+
+float fbw_setting_damper_yaw = 0.0f;
+float fbw_setting_damper_pitch = 0.0f;
+
+
+void fbw_settings_init(void)
+ {
+   fbw_setting_damper_yaw = 0.0f;
+   fbw_setting_damper_pitch = 0.0f;
+
+ }
+
+
