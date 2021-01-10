c6ed3e5522354e9995bc1adeaabda75794853477
danielwillemsen <danielwillemsen@live.nl>
2021-01-10 17:02:08 +0100

    Working version, needs tuning for the opticflow.

--- sw/airborne/modules/divergence_stop/divergence_stop.c
+++ sw/airborne/modules/divergence_stop/divergence_stop.c
@@ -5,11 +5,22 @@
 #include "subsystems/abi.h"
 #include <stdio.h>
 #include <time.h>
+#include "mcu_periph/sys_time.h"
 
 #ifndef DIVERGENCE_STOP_THRESHOLD
 #error Error! divergence_stop threshold not defined
 #endif
-float size_divergence;
+
+#ifndef DIVERGENCE_STOP_AVOIDANCE_PITCH
+#error Error! divergence_stop avoidance pitch not defined
+#endif
+
+#ifndef DIVERGENCE_STOP_AVOIDANCE_DURATION
+#error Error! divergence_stop avoidance duration not defined
+#endif
+
+
+
 
 #ifndef OFH_OPTICAL_FLOW_ID
 #define OFH_OPTICAL_FLOW_ID ABI_BROADCAST
@@ -17,8 +28,28 @@ float size_divergence;
 
 #define PRINT(string,...) fprintf(stderr, "[divergence_stop->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
 
-int navigation_state = DIVERGENCE_MODE_FORWARD;
-float divergence_stop_threshold = DIVERGENCE_STOP_THRESHOLD;
+#include "state.h"
+#include "subsystems/radio_control.h"
+#include "firmwares/rotorcraft/stabilization.h"
+#include "firmwares/rotorcraft/stabilization/stabilization_attitude.h"
+#include "firmwares/rotorcraft/stabilization/stabilization_attitude_rc_setpoint.h"
+#include "autopilot.h"
+
+// Own Variables
+float divergence_stop_threshold;
+float divergence_stop_avoidance_pitch;
+float divergence_stop_avoidance_duration;
+
+float size_divergence;
+
+struct ctrl_module_demo_struct {
+// RC Inputs
+  struct Int32Eulers rc_sp;
+
+// Output command
+  struct Int32Eulers cmd;
+
+} ctrl;
 
 static abi_event optical_flow_ev;
 static void optical_flow_cb(uint8_t sender_id __attribute__((unused)), uint32_t stamp, int16_t flow_x, int16_t flow_y,
@@ -30,7 +61,10 @@ static void optical_flow_cb(uint8_t sender_id __attribute__((unused)), uint32_t 
 void divergence_stop_init(void)
 {
   // Initialize by flying forward
-  navigation_state = DIVERGENCE_MODE_FORWARD;
+  last_time_exceeded =get_sys_time_float() - 99;
+  divergence_stop_threshold = DIVERGENCE_STOP_THRESHOLD;
+  divergence_stop_avoidance_pitch = DIVERGENCE_STOP_AVOIDANCE_PITCH;
+  divergence_stop_avoidance_duration = DIVERGENCE_STOP_AVOIDANCE_DURATION;
 
   // Subscribe to the optical flow estimator:
   AbiBindMsgOPTICAL_FLOW(OFH_OPTICAL_FLOW_ID, &optical_flow_ev, optical_flow_cb);
@@ -40,16 +74,46 @@ void divergence_stop_init(void)
 void divergence_stop_periodic(void)
 {
   if (size_divergence > divergence_stop_threshold) {
-	  navigation_state = DIVERGENCE_MODE_STOP;
-  } else {
-	  navigation_state = DIVERGENCE_MODE_FORWARD;
-  }
-  if (navigation_state == DIVERGENCE_MODE_STOP) {
-	  // guidance_h_set_guided_body_vel(0, 0);
+          last_time_exceeded = get_sys_time_float();
 	  PRINT("THRESHOLD EXCEEDED\n");
-  } //else {
-	  // guidance_h_set_guided_body_vel(1.0, 0);
-  //}
-  PRINT("%f\n", size_divergence);
+  }
   return;
+}
+
+////////////////////////////////////////////////////////////////////
+// Call our controller
+// Implement own Horizontal loops
+void guidance_h_module_init(void)
+{
+}
+
+void guidance_h_module_enter(void)
+{
+  // Store current heading
+  ctrl.cmd.psi = stateGetNedToBodyEulers_i()->psi;
+
+  // Convert RC to setpoint
+  stabilization_attitude_read_rc_setpoint_eulers(&ctrl.rc_sp, autopilot.in_flight, false, false);
+}
+
+void guidance_h_module_read_rc(void)
+{
+  stabilization_attitude_read_rc_setpoint_eulers(&ctrl.rc_sp, autopilot.in_flight, false, false);
+}
+
+
+void guidance_h_module_run(bool in_flight)
+{
+  float pitch = RadOfDeg(0.);
+  if (get_sys_time_float() - last_time_exceeded < divergence_stop_avoidance_duration) {
+      pitch = RadOfDeg(divergence_stop_avoidance_pitch);
+  }
+
+  ctrl.cmd.phi = ctrl.rc_sp.phi;
+  ctrl.cmd.theta = ANGLE_BFP_OF_REAL(pitch) + ctrl.rc_sp.theta;
+
+  stabilization_attitude_set_rpy_setpoint_i(&(ctrl.cmd));
+  stabilization_attitude_run(in_flight);
+
+  // Alternatively, use the indi_guidance and send AbiMsgACCEL_SP to it instead of setting pitch and roll
 }
--- sw/airborne/modules/divergence_stop/divergence_stop.h
+++ sw/airborne/modules/divergence_stop/divergence_stop.h
@@ -1,17 +1,29 @@
 #ifndef DIVERGENCE_STOP_H
 #define DIVERGENCE_STOP_H
 
-extern void divergence_stop_init(void);
-extern void divergence_stop_periodic(void);
-#ifndef DIVERGENCE_MODE_FORWARD
-#define DIVERGENCE_MODE_FORWARD 1
-#endif
+#include <std.h>
 
-#ifndef DIVERGENCE_MODE_STOP
-#define DIVERGENCE_MODE_STOP 0
-#endif
+// Demo with own guidance_h
+#define GUIDANCE_H_MODE_MODULE_SETTING GUIDANCE_H_MODE_MODULE
+
+// But re-using an existing altitude-hold controller
+#define GUIDANCE_V_MODE_MODULE_SETTING GUIDANCE_V_MODE_RC_DIRECT
 
 extern float divergence_stop_threshold;
-extern int navigation_state;
+extern float divergence_stop_avoidance_pitch;
+extern float divergence_stop_avoidance_duration;
+float last_time_exceeded;
+
+// Implement own Horizontal loops
+extern void guidance_h_module_init(void);
+extern void guidance_h_module_enter(void);
+extern void guidance_h_module_read_rc(void);
+extern void guidance_h_module_run(bool in_flight);
+
+extern void divergence_stop_init(void);
+extern void divergence_stop_periodic(void);
+
 
 #endif
+
+
