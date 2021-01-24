04af1649b39decec37cd1daf803784b370155e01
matteobarbera <matteo.barbera97@gmail.com>
2021-01-24 16:04:33 +0100

    files for PDF exercise

---
+++ sw/airborne/modules/mav_course_exercise/mav_exercise.h
@@ -0,0 +1,11 @@
+//
+// Created by matteo on 31/05/2020.
+//
+
+#ifndef PAPARAZZI_MAV_EXERCISE_H
+#define PAPARAZZI_MAV_EXERCISE_H
+
+extern void mav_exercise_init(void);
+extern void mav_exercise_periodic(void);
+
+#endif //PAPARAZZI_MAV_EXERCISE_H
