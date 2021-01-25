31e8e2d3aae96c0b5b432cc938b31aa45b36cb05
matteobarbera <matteo.barbera97@gmail.com>
2021-01-25 10:03:49 +0100

    Description and comments fixes

--- sw/airborne/modules/mav_course_exercise/mav_exercise.c
+++ sw/airborne/modules/mav_course_exercise/mav_exercise.c
@@ -1,6 +1,22 @@
-//
-// Created by matteo on 31/05/2020.
-//
+/*
+ * Copyright (C) 2021 Matteo Barbera <matteo.barbera97@gmail.com>
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 #include "mav_exercise.h"
 #include "subsystems/abi.h"
