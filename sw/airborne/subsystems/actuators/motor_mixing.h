aec4dd47c022cafb4e068a01ca61b133accd8fc7
TML De Ponti <tomasodp@gmail.com>
2021-07-13 11:22:28 +0200

    Disable pitch control with motors when taking off from the rope

--- sw/airborne/subsystems/actuators/motor_mixing.h
+++ sw/airborne/subsystems/actuators/motor_mixing.h
@@ -47,5 +47,6 @@ extern struct MotorMixing motor_mixing;
 extern void motor_mixing_init(void);
 extern void motor_mixing_run(bool motors_on, bool override_on, pprz_t in_cmd[]);
 extern void motor_mixing_run_spinup(uint32_t counter, uint32_t max_counter);
+extern bool pitch_control;
 
 #endif /* MOTOR_MIXING_H */
