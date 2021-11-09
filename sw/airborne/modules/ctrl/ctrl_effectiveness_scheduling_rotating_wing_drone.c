80b9f63cc617b0afabaea56468c9a63d616e93f6
Dennis van Wijngaarden <wijngaarden.dennis@gmail.com>
2021-11-09 09:55:56 +0100

    Bugfixes for rot_wing_effectiveness_scheduler

--- sw/airborne/modules/ctrl/ctrl_effectiveness_scheduling_rotating_wing_drone.c
+++ sw/airborne/modules/ctrl/ctrl_effectiveness_scheduling_rotating_wing_drone.c
@@ -41,14 +41,22 @@ float rot_wing_y_length = ROT_WING_SCHED_Y_ARM_LENGTH;
 #endif
 
 // Define g1 p and q values for roll and pitch for 0 and 90 degrees rotation angles for the right and left motors
+float rot_wing_initial_g1_p[INDI_OUTPUTS] = STABILIZATION_INDI_G1_ROLL;
+
 float rot_wing_g1_p_0[2];
 float rot_wing_g1_q_90[2] = ROT_WING_SCHED_G1_Q_90; 
+
+// Define control effectiveness variables in roll and pitch for side motors
+float rot_wing_g1_p_side_motors[2];
+float rot_wing_g1_q_side_motors[2];
 
 #if PERIODIC_TELEMETRY
 #include "subsystems/datalink/telemetry.h"
 static void send_rot_wing(struct transport_tx *trans, struct link_device *dev)
 {
-  pprz_msg_send_ROT_WING(trans, dev, AC_ID, &rot_wing_angle_deg);
+  pprz_msg_send_ROT_WING(trans, dev, AC_ID, &rot_wing_angle_deg,
+                          2, rot_wing_g1_p_side_motors,
+                          2, rot_wing_g1_q_side_motors);
 }
 #endif
 
@@ -57,8 +65,8 @@ void ctrl_eff_scheduling_rotating_wing_drone_init(void)
   // init scheduling
 
   // Copy initial effectiveness on roll for side motors
-  rot_wing_g1_p_0[0] = g1g2[0][1];
-  rot_wing_g1_p_0[1] = g1g2[1][3];
+  rot_wing_g1_p_0[0] = rot_wing_initial_g1_p[1];
+  rot_wing_g1_p_0[1] = rot_wing_initial_g1_p[3];
 
   #if PERIODIC_TELEMETRY
     register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_ROT_WING, send_rot_wing);
@@ -72,14 +80,20 @@ void ctrl_eff_scheduling_rotating_wing_drone_periodic(void)
 
   // Pitch and Roll effectiveness of side motors (motor 2 right(idx = 1), motor 4 left (idx = 3)) should be updated based on the angle of the rotating wing
   float rot_wing_angle_rad = (float)rot_wing_angle_deg / 180. * M_PI;
-  float c_rot_wing_angle = cos(rot_wing_angle_rad);
-  float s_rot_wing_angle = sin(rot_wing_angle_rad);
+  float c_rot_wing_angle = cosf(rot_wing_angle_rad);
+  float s_rot_wing_angle = sinf(rot_wing_angle_rad);
 
-  g1g2[0][1] = c_rot_wing_angle * rot_wing_g1_p_0[0] / INDI_G_SCALING; // Roll eff right motor
-  g1g2[0][3] = c_rot_wing_angle * rot_wing_g1_p_0[1] / INDI_G_SCALING; // Roll eff left motor
+  rot_wing_g1_p_side_motors[0] = c_rot_wing_angle * rot_wing_g1_p_0[0] / INDI_G_SCALING;
+  rot_wing_g1_p_side_motors[1] = c_rot_wing_angle * rot_wing_g1_p_0[1] / INDI_G_SCALING;
 
-  g1g2[1][1] = s_rot_wing_angle * rot_wing_g1_q_90[0] / INDI_G_SCALING; // Pitch eff right motor
-  g1g2[1][3] = s_rot_wing_angle * rot_wing_g1_q_90[1] / INDI_G_SCALING; // Pitch eff left motor
+  rot_wing_g1_q_side_motors[0] = s_rot_wing_angle * rot_wing_g1_q_90[0] / INDI_G_SCALING;
+  rot_wing_g1_q_side_motors[1] = s_rot_wing_angle * rot_wing_g1_q_90[1] / INDI_G_SCALING;
+
+  g1g2[0][1] = rot_wing_g1_p_side_motors[0]; // Roll eff right motor
+  g1g2[0][3] = rot_wing_g1_p_side_motors[1]; // Roll eff left motor
+
+  g1g2[1][1] = rot_wing_g1_q_side_motors[0]; // Pitch eff right motor
+  g1g2[1][3] = rot_wing_g1_q_side_motors[1]; // Pitch eff left motor
 }
 
 
