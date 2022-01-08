efe03f2c709ade538c351280f8e47a118bed609f
gervaselovellprescod <gervase.prescod@gmail.com>
2022-01-08 13:43:38 +0100

    [sw] added my .h file for the control effectiveness calculator

---
+++ sw/airborne/modules/ctrl/ctrl_effectiveness_calculator.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 20121 Gervase Lovell-Prescod <gervase.prescod@gmail.com>
+ */
+
+/**
+ * @file modules/ctrl/ctrl_effectiveness_scheduling.h
+ */
+
+#ifndef CTRL_EFFECTIVENESS_CALCULATOR_H
+#define CTRL_EFFECTIVENESS_CALCULATOR_H
+
+#include "generated/airframe.h"
+#include "firmwares/rotorcraft/stabilization/stabilization_indi.h"
+
+extern void ctrl_eff(void);
+
+#endif  /* CTRL_EFFECTIVENESS_CALCULATOR_H */
