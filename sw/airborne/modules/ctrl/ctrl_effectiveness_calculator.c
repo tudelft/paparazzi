28116ad2cd923c10594e61834c41e9474068296c
gervaselovellprescod <gervase.prescod@gmail.com>
2022-01-24 17:04:28 +0100

    [sw] altered ctrl_effectiveness_calculator.c to accept drone specific metrics from airframe file

--- sw/airborne/modules/ctrl/ctrl_effectiveness_calculator.c
+++ sw/airborne/modules/ctrl/ctrl_effectiveness_calculator.c
@@ -20,24 +20,6 @@ static float ab_to_cd(float x);
  */
 void ctrl_eff(void)
 {
-
-	/**
-	 * Motor coefficients. Thrust as a function of ESC command (on range [0, 9600]) T = k1 x^2 + k2 x + k3
-	 */
-	float k1 =  0.00000002;
-	float k2 = 0.0003;
-	float k3 = -0.2157;
-
-	/**
-	 * Drone specific metrics
-	 */
-	float b = 0.3;			// distance in y-axis from c.g. [m]
-	float l = 0.1;			// distance in x-axis from c.g.	[m]
-    float m = 1.3;			// Mass							[kg]
-    float I_XX = 0.017897;	// Moment of Inertia 			[kg m2]
-    float I_YY = 0.003390;	// 			"					[kg m2]
-    float I_ZZ = 0.020337;	// 			"					[kg m2]
-
     /**
      * Some definitions for ease of reading. In the future I want to use angular acceleration of motor so labeled as omega already
      */
@@ -46,22 +28,22 @@ void ctrl_eff(void)
     float omega_r0 = actuator_state_filt_vect[2];
     float omega_l0 = actuator_state_filt_vect[3];
 
-    float ctrl_deriv_00 = -b * sinf(theta_l0) * (k1 * omega_l0 * omega_l0 + k2 * omega_l0 + k3) * (1 / I_XX) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_01 =  b * sinf(theta_r0) * (k1 * omega_r0 * omega_r0 + k2 * omega_r0 + k3) * (1 / I_XX) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_02 = -b * cosf(theta_r0) * (2 * k1 * omega_r0 + k2) * (1 / I_XX);
-    float ctrl_deriv_03 =  b * cosf(theta_l0) * (2 * k1 * omega_l0 + k2) * (1 / I_XX);
-    float ctrl_deriv_10 =  l * cosf(theta_l0) * (k1 * omega_l0 * omega_l0 + k2 * omega_l0 + k3) * (1 / I_YY) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_11 =  l * cosf(theta_r0) * (k1 * omega_r0 * omega_r0 + k2 * omega_r0 + k3) * (1 / I_YY) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_12 =  l * sinf(theta_r0) * (2 * k1 * omega_r0 + k2) * (1 / I_YY);
-    float ctrl_deriv_13 =  l * sinf(theta_l0) * (2 * k1 * omega_l0 + k2) * (1 / I_YY);
-    float ctrl_deriv_20 = -b * cosf(theta_l0) * (k1 * omega_l0 * omega_l0 + k2 * omega_l0 + k3) * (1 / I_ZZ) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_21 =  b * cosf(theta_r0) * (k1 * omega_r0 * omega_r0 + k2 * omega_r0 + k3) * (1 / I_ZZ) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_22 =  b * sinf(theta_r0) * (2 * k1 * omega_r0 + k2) * (1 / I_ZZ);
-    float ctrl_deriv_23 = -b * sinf(theta_l0) * (2 * k1 * omega_l0 + k2) * (1 / I_ZZ);
-    float ctrl_deriv_30 = (k1 * omega_l0 * omega_l0 + k2 * omega_l0 + k3) * sinf(theta_l0) * (1/m) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_31 = (k1 * omega_r0 * omega_r0 + k2 * omega_r0 + k3) * sinf(theta_r0) * (1/m) * (1/(float)MAX_PPRZ);
-    float ctrl_deriv_32 =  -(2 * k1 * omega_r0 + k2) * cosf(theta_r0) * (1/m);
-    float ctrl_deriv_33 =  -(2 * k1 * omega_l0 + k2) * cosf(theta_l0) * (1/m);
+    float ctrl_deriv_00 = -Y_DIST * sinf(theta_l0) * (K1 * omega_l0 * omega_l0 + K2 * omega_l0 + K3) * (1 / I_XX) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_01 =  Y_DIST * sinf(theta_r0) * (K1 * omega_r0 * omega_r0 + K2 * omega_r0 + K3) * (1 / I_XX) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_02 = -Y_DIST * cosf(theta_r0) * (2 * K1 * omega_r0 + K2) * (1 / I_XX);
+    float ctrl_deriv_03 =  Y_DIST * cosf(theta_l0) * (2 * K1 * omega_l0 + K2) * (1 / I_XX);
+    float ctrl_deriv_10 =  X_DIST * cosf(theta_l0) * (K1 * omega_l0 * omega_l0 + K2 * omega_l0 + K3) * (1 / I_YY) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_11 =  X_DIST * cosf(theta_r0) * (K1 * omega_r0 * omega_r0 + K2 * omega_r0 + K3) * (1 / I_YY) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_12 =  X_DIST * sinf(theta_r0) * (2 * K1 * omega_r0 + K2) * (1 / I_YY);
+    float ctrl_deriv_13 =  X_DIST * sinf(theta_l0) * (2 * K1 * omega_l0 + K2) * (1 / I_YY);
+    float ctrl_deriv_20 = -Y_DIST * cosf(theta_l0) * (K1 * omega_l0 * omega_l0 + K2 * omega_l0 + K3) * (1 / I_ZZ) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_21 =  Y_DIST * cosf(theta_r0) * (K1 * omega_r0 * omega_r0 + K2 * omega_r0 + K3) * (1 / I_ZZ) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_22 =  Y_DIST * sinf(theta_r0) * (2 * K1 * omega_r0 + K2) * (1 / I_ZZ);
+    float ctrl_deriv_23 = -Y_DIST * sinf(theta_l0) * (2 * K1 * omega_l0 + K2) * (1 / I_ZZ);
+    float ctrl_deriv_30 = (K1 * omega_l0 * omega_l0 + K2 * omega_l0 + K3) * sinf(theta_l0) * (1/MASS) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_31 = (K1 * omega_r0 * omega_r0 + K2 * omega_r0 + K3) * sinf(theta_r0) * (1/MASS) * (1/(float)MAX_PPRZ);
+    float ctrl_deriv_32 =  -(2 * K1 * omega_r0 + K2) * cosf(theta_r0) * (1/MASS);
+    float ctrl_deriv_33 =  -(2 * K1 * omega_l0 + K2) * cosf(theta_l0) * (1/MASS);
 
 
     g1g2[0][0] = ctrl_deriv_00;
@@ -82,11 +64,11 @@ void ctrl_eff(void)
     g1g2[3][3] = ctrl_deriv_33;
 
 
-//    printf("\n");
-//    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_00, ctrl_deriv_01, ctrl_deriv_02, ctrl_deriv_03);
-//    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_10, ctrl_deriv_11, ctrl_deriv_12, ctrl_deriv_13);
-//    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_20, ctrl_deriv_21, ctrl_deriv_22, ctrl_deriv_23);
-//    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_30, ctrl_deriv_31, ctrl_deriv_32, ctrl_deriv_33);
+    printf("\n");
+    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_00, ctrl_deriv_01, ctrl_deriv_02, ctrl_deriv_03);
+    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_10, ctrl_deriv_11, ctrl_deriv_12, ctrl_deriv_13);
+    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_20, ctrl_deriv_21, ctrl_deriv_22, ctrl_deriv_23);
+    printf("\n%f\t%f\t%f\t%f", ctrl_deriv_30, ctrl_deriv_31, ctrl_deriv_32, ctrl_deriv_33);
 
 }
 
