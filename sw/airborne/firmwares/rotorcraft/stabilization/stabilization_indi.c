ecbd66ddb59b38d71fd7981295e542a173a617f3
tmldeponti <tomasodp@gmail.com>
2022-04-11 20:38:47 +0200

    changed preferred state of aero surf to be 0

--- sw/airborne/firmwares/rotorcraft/stabilization/stabilization_indi.c
+++ sw/airborne/firmwares/rotorcraft/stabilization/stabilization_indi.c
@@ -571,10 +571,10 @@ void stabilization_indi_rate_run(struct FloatRates rate_sp, bool in_flight)
     du_min[i] = -MAX_PPRZ * act_is_servo[i] - actuator_state_filt_vect[i];
     du_max[i] = MAX_PPRZ - actuator_state_filt_vect[i];
     du_pref[i] = act_pref[i] - actuator_state_filt_vect[i];
-    if (act_is_servo[i])
-    {
-      du_pref[i] = 0;
-    }
+    // if (act_is_servo[i])
+    // {
+    //   du_pref[i] = 0;
+    // }
 
     if (doublet_active)
     { if(i==doublet_axis){
