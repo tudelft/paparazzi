ac80def837c9f764f2041939c58f2f61493abdf4
gervaselovellprescod <gervase.prescod@gmail.com>
2022-05-24 09:31:47 +0200

    minor changes

--- sw/airborne/modules/ctrl/ctrl_effectiveness_calculator.c
+++ sw/airborne/modules/ctrl/ctrl_effectiveness_calculator.c
@@ -44,8 +44,8 @@ void ctrl_eff(void)
     float theta_r0 = pprz_to_theta_right(actuator_state_filt_vect[1]);
 //    float omega_r0 = actuator_state_filt_vect[2];
 //    float omega_l0 = actuator_state_filt_vect[3];
-    float omega_r0 = actuator_state_filt_vect[2] < 4000 ? 4000 : actuator_state_filt_vect[2];
-    float omega_l0 = actuator_state_filt_vect[3] < 4000 ? 4000 : actuator_state_filt_vect[3];
+    float omega_r0 = actuator_state_filt_vect[2] < 2000 ? 2000 : actuator_state_filt_vect[2];
+    float omega_l0 = actuator_state_filt_vect[3] < 2000 ? 2000 : actuator_state_filt_vect[3];
 
     float ctrl_deriv_00 = -Y_DIST * sinf(theta_l0) * (K1 * omega_l0 * omega_l0 + K2 * omega_l0 + K3) * (1 / I.xx) * (1/(float)MAX_PPRZ);
     float ctrl_deriv_01 =  Y_DIST * sinf(theta_r0) * (K1 * omega_r0 * omega_r0 + K2 * omega_r0 + K3) * (1 / I.xx) * (1/(float)MAX_PPRZ);
