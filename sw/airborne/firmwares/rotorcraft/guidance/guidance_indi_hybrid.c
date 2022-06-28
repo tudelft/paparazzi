89dbc5ae7e3d4523b69996d900bb7d1ff3c662d9
Ewoud Smeur <e.j.j.smeur@tudelft.nl>
2022-06-28 11:39:41 +0200

    change servo effectiveness

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
@@ -567,7 +567,7 @@ float guidance_indi_get_liftd(float airspeed, float theta) {
   if(airspeed < 12) {
     float pitch_interp = DegOfRad(theta);
     Bound(pitch_interp, -80.0, -20.0);
-    float ratio = (pitch_interp + 40.0)/(-40.);
+    float ratio = (pitch_interp + 20.0)/(-60.);
     liftd = -24.0*ratio*gih_params.lift_pitch_eff/0.12;
   } else {
     liftd = -(airspeed - 8.5)*gih_params.lift_pitch_eff/M_PI*180.0;
