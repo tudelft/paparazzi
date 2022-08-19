5bb0fe386e6ab13dfd74fbc38034624f6fda03ff
Freek van Tienen <freek.v.tienen@gmail.com>
2022-08-19 22:59:38 +0200

    UAVCAN fixes

--- sw/airborne/arch/chibios/modules/uavcan/uavcan.c
+++ sw/airborne/arch/chibios/modules/uavcan/uavcan.c
@@ -103,12 +103,11 @@ static THD_FUNCTION(uavcan_rx, p)
   struct uavcan_iface_t *iface = (struct uavcan_iface_t *)p;
 
   chRegSetThreadName("uavcan_rx");
-  chEvtRegister(&iface->can_driver->rxfull_event, &el, EVENT_MASK(0));
+  chEvtRegister(&iface->can_driver->rxfull_event, &el, 0);
   while (true) {
     if (chEvtWaitAnyTimeout(ALL_EVENTS, TIME_MS2I(100)) == 0) {
       continue;
     }
-    chMtxLock(&iface->mutex);
 
     // Wait until a CAN message is received
     while (canReceive(iface->can_driver, CAN_ANY_MAILBOX, &rx_msg, TIME_IMMEDIATE) == MSG_OK) {
@@ -131,9 +130,10 @@ static THD_FUNCTION(uavcan_rx, p)
 #endif
 
       // Let canard handle the frame
+      chMtxLock(&iface->mutex);
       canardHandleRxFrame(&iface->canard, &rx_frame, timestamp);
+      chMtxUnlock(&iface->mutex);
     }
-    chMtxUnlock(&iface->mutex);
   }
   chEvtUnregister(&iface->can_driver->rxfull_event, &el);
 }
@@ -148,9 +148,9 @@ static THD_FUNCTION(uavcan_tx, p)
   uint8_t err_cnt = 0;
 
   chRegSetThreadName("uavcan_tx");
-  chEvtRegister(&iface->can_driver->txempty_event, &txc, EVENT_MASK(0));
-  chEvtRegister(&iface->can_driver->error_event, &txe, EVENT_MASK(1));
-  chEvtRegister(&iface->tx_request, &txr, EVENT_MASK(2));
+  chEvtRegister(&iface->can_driver->txempty_event, &txc, 0);
+  chEvtRegister(&iface->can_driver->error_event, &txe, 1);
+  chEvtRegister(&iface->tx_request, &txr, 2);
 
   while (true) {
     eventmask_t evts = chEvtWaitAnyTimeout(ALL_EVENTS, TIME_MS2I(100));
@@ -161,7 +161,7 @@ static THD_FUNCTION(uavcan_tx, p)
 
     // Transmit error
     if (evts & EVENT_MASK(1)) {
-      //iface->transmit_err_cnt++;
+      iface->transmit_err_cnt++;
       chEvtGetAndClearFlags(&txe);
       continue;
     }
