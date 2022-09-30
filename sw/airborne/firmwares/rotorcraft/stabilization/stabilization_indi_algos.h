cecee4c30bb63c8ce6f1448f7a7d9d8573f417f1
Till Blaha <t.m.blaha@student.tudelft.nl>
2022-09-30 15:44:46 +0200

    first version of a flight plan

--- sw/airborne/firmwares/rotorcraft/stabilization/stabilization_indi_algos.h
+++ sw/airborne/firmwares/rotorcraft/stabilization/stabilization_indi_algos.h
@@ -35,6 +35,7 @@ extern struct Int32Quat   stab_att_sp_quat;  ///< with #INT32_QUAT_FRAC
 extern struct Int32Eulers stab_att_sp_euler; ///< with #INT32_ANGLE_FRAC
 extern float g1g2[INDI_OUTPUTS][INDI_NUM_ACT];
 extern float actuator_state_filt_vect[INDI_NUM_ACT];
+extern float indi_v[INDI_OUTPUTS];
 
 extern bool indi_use_adaptive;
 extern activeSetAlgoChoice indi_ctl_alloc_algo;
