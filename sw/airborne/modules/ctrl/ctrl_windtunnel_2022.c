373ec860a453ee6ba9ecade977e2021d433fd171
tmldeponti <tomasodp@gmail.com>
2022-11-18 13:33:25 +0100

    added aoa to windtunnel message. Settings for es1

--- sw/airborne/modules/ctrl/ctrl_windtunnel_2022.c
+++ sw/airborne/modules/ctrl/ctrl_windtunnel_2022.c
@@ -36,21 +36,22 @@
 #include "mcu_periph/sys_time.h"
 #include "modules/wind_tunnel/wind_tunnel_rot_wing.h"
 #include "stdio.h"
+#include "modules/sensors/aoa_pwm.h"
 //#######################################################################################################################################################################################
 
 // Defines of time variables ##########################################################################################################################################################
 float dt_s = 1;// [s] Short test time interval. Used for procedures which do not require much time. (e.g. 1s)
-float dt_m = 3;// [s] Medium test time interval. Used for procedures which involve transitional states (e.g. actuator test, 5s)
-float dt_l = 3;// [s] Long test time interval. Used for procedure which involve the use of slow dynamics (e.g. rotation wing, 6s)
+float dt_m = 5;// [s] Medium test time interval. Used for procedures which involve transitional states (e.g. actuator test, 5s)
+float dt_l = 5;// [s] Long test time interval. Used for procedure which involve the use of slow dynamics (e.g. rotation wing, 6s)
 //#######################################################################################################################################################################################
 
 // Defines of nested loop iteration limits ##############################################################################################################################################
-#define imax 7 // Number of Wing set point 
-#define jmax 3 // Number of Motor status (e.g. 5)
+#define imax 4 // Number of Wing set point 
+#define jmax 12 // Number of Motor status (e.g. 5)
 #define mmax 5 // Number of Motors used in the Motor status (e.g. 4) 
 #define kmax 8 // Number of Aerodynamic Surfaces + Motors tested  (e.g. 4)
-#define nmax 8 // Number of Excitation steps 
-#define asel 4 // Number of selected actuators
+#define nmax 4 // Number of Excitation steps 
+#define asel 1 // Number of selected actuators
 //#######################################################################################################################################################################################
 
 // Defines of variables to be used in manual slider test of actuators ###################################################################################################################
@@ -68,25 +69,26 @@ int16_t push_static = 0;
 
 // Defines of tested excitation signals #################################################################################################################################################
 //int16_t mot_status[jmax][mmax] = {{0,0,0,0},{1000,1000,1000,1000},{2000,2000,2000,2000},{3000,3000,3000,3000}}; // [pprz] Motor status define [Status][Motor]
-int16_t mot_status[jmax][mmax] = {{0,0,0,0,0},{3000,3000,3000,3000,0},{0,0,0,0,3000}}; // [pprz] Motor status define [Status][Motor]
+int16_t mot_status[jmax][mmax] = {{0,0,0,0,0},{3000,3000,3000,3000,0},{5320,5320,5320,5320,0},{0,0,0,0,3000},{3000,3000,3000,3000,3000},{5320,5320,5320,5320,3000},{0,0,0,0,4500},{3000,3000,3000,3000,4500},{5320,5320,5320,5320,4500},{0,0,0,0,6000},{3000,3000,3000,3000,6000},{5320,5320,5320,5320,6000}}; // [pprz] Motor status define [Status][Motor]
 //int16_t as_static[nmax] = {-9600,-4800,1920,4800,9600}; // [pprz] Excitation signals Aerodynamic Surface 
 //int16_t mot_static[nmax] = {2000,4000,5000,6000,8000};  // [pprz] Excitation signals Motors
 int16_t gen_static[kmax][nmax+3] = {{-9600,-4800,1920 ,4800 ,9600  , 0   , 0   , 0   },         // [pprz] ail l
                                     {-9600,-4800,1920 ,4800 ,9600  , 0   , 0   , 0   },         // [pprz] ail r
-                                    {-9600,-7000,-4800,-3500,-1920 , 1920, 4800, 9600},         // [pprz] ele
+                                    {-9600,0,9600,0   ,-1920 , 1920, 4800, 9600        },         // [pprz] ele
                                     {-9600,-4800,1920 ,4800 ,9600  , 0   , 0   , 0   },         // [pprz] rud
-                                    {2000 ,4000 ,5000 ,6000 ,8000  , 0   , 0   , 0   },         // [pprz] mot 0
-                                    {2000 ,4000 ,5000 ,6000 ,8000  , 0   , 0   , 0   },         // [pprz] mot 1
-                                    {2000 ,4000 ,5000 ,6000 ,8000  , 0   , 0   , 0   },         // [pprz] mot 2
-                                    {2000 ,4000 ,5000 ,6000 ,8000  , 0   , 0   , 0   }} ;       // [pprz] mot 3
+                                    {1000 ,3000 ,5000 ,7000 ,9000  , 0   , 0   , 0   },         // [pprz] mot 0
+                                    {1000 ,3000 ,5000 ,7000 ,9000  , 0   , 0   , 0   },         // [pprz] mot 1
+                                    {1000 ,3000 ,5000 ,7000 ,9000  , 0   , 0   , 0   },         // [pprz] mot 2
+                                    {1000 ,3000 ,5000 ,7000 ,9000  , 0   , 0   , 0   }} ;       // [pprz] mot 3
 int16_t sync_cmd[6] = {800,0,1000,0,1200,0};            // [pprz] Excitation signals pusher motor during syncing procedure
 int16_t push_cmd[2] = {2000,4000};
-int8_t selected_act_idx[asel] = {3,9,10,7};              // Array of indeces of selected actuators
+int8_t selected_act_idx[asel] = {9};              // Array of indeces of selected actuators
 int8_t tail_act_idx[2] = {9,10};              // Array of indeces of selected actuators
-float wing_sp[imax] = {0,10,30,45,60,75,90};            // [deg] Tested skew angles 
+float wing_sp[imax] = {15,30,60,75};            // [deg] Tested skew angles 
 float rotation_rate_sp[6] = {0.15,0.15,0.20,0.20,0.25,0.25};
 float boa[6] =  {90,0,90,0,90,0};
-float ramp_ratio = 1./3.;                               // [%] Percentage of test time dedicated to gradual sweep. FLOAT!
+float ramp_ratio = 1.;                               // [%] Percentage of test time dedicated to gradual sweep. FLOAT!
+float ramp_ratio_ms = 1./3.;                               // [%] Percentage of test time dedicated to gradual sweep. FLOAT!
 float max_rotation_rate=3.14/2.; // same                 // [rad/s] Maximum rotational velocity of wing
 //#######################################################################################################################################################################################
 
@@ -97,9 +99,12 @@ static float t_excitation = 0;  // [s]Time of start of excitation of actuator
 static float t_mot_status = 0;  // [s]
 static float t_wing = 0;        // [s]
 static float t_sync = 0;        // [s]
-static float t_skew = 0;        // [s]     
+static float t_skew = 0;        // [s]   
+static float t_test = 0;        // [s]  
 float stopwatch = 0;            // [s] Elapsed time from start of excitation
 float ratio_excitation = 1;     // [%] Percentage of completion of sweep for excitation of actuator
+float tcp = 0;                  // [%] Percentage of completion of test
+float aoa_wt = 0;               // [rad] Angle of Attack at Elevator
 
 // BOOLs --------------------------------------------------------------------------------------------
 bool done_wing = true;          // Skew test completed. Go to next skew.
@@ -112,7 +117,7 @@ bool shut_off = true;           // Shutoff of actuator completed. Go to next act
 bool static_test = false;       // 
 bool test_skew_active = false;  // Automatic skew test is active. [experimental skew test]
 bool done_skew = true;          // Done skew. Go to next skew [experimetnal skew test]
-bool single_act = false;        // Turn on to select only certain actuators
+bool single_act = true;        // Turn on to select only certain actuators
 bool verbose_test_ID = false;   // When true, record test ID
 // INTEGERs ------------------------------------------------------------------------------------------
 int8_t i = 0;                                     // Wing set point counter
@@ -130,7 +135,7 @@ int16_t cmd_0 = 0;                                // [pprz] Initial CMD of actua
 int16_t cmd_target = 0;                           // [pprz] Excitation cmd target
 int16_t cmd_0_mot_status[mmax] = {0,0,0,0};       // [pprz] Initial CMD of mot status
 // STRINGs --------------------------------------------------------------------------------------------
-char test_id[5] = "AE1";
+char test_id[5] = "ES1";
 char point_id[20] = "Blank";
 //#######################################################################################################################################################################################
 
@@ -197,6 +202,7 @@ bool skew_moment(void){
 
 // Function to start a windtunnel test ##################################################################################################################################################
 bool windtunnel_control(void){
+  if (!test_active){t_test = get_sys_time_float();}
  static_test = true;                            
  test_active = true;
  motors_on_wt = true;                           // Arm motors
@@ -210,6 +216,8 @@ bool windtunnel_control(void){
         test_active = false;                    // if done skewing terminate the test
         motors_on_wt = false;
         p += 1;
+        printf("Total Test Time %0.1f [s]",get_sys_time_float()-t_test);
+        tcp = 0;
         return false;}}}
 //#######################################################################################################################################################################################
 
@@ -264,7 +272,7 @@ bool mot_status_control(void){
         done_mot_status = true;
         j += 1;}}
      else{
-      ratio_excitation = stopwatch / dt_m / ramp_ratio;    // Calclate the percentage of completion of the initial linear sweep
+      ratio_excitation = stopwatch / dt_m / ramp_ratio_ms;    // Calclate the percentage of completion of the initial linear sweep
       Bound(ratio_excitation, 0, 1);
       if (ratio_excitation==1){
         verbose_test_ID=true;
@@ -279,7 +287,7 @@ bool mot_status_control(void){
       return true;
      }else {
       stopwatch = get_sys_time_float() - t_mot_status;
-      ratio_excitation = stopwatch / dt_m / ramp_ratio;
+      ratio_excitation = stopwatch / dt_m / ramp_ratio_ms;
       Bound(ratio_excitation, 0, 1);
       for ( int8_t m = 0; m < mmax; m++){actuators_wt[m] = (int16_t) cmd_0_mot_status[m] + (0- cmd_0_mot_status[m]) * ratio_excitation;}
       if (ratio_excitation == 1) {                                              // If shutoff completed move to next actuator
@@ -298,11 +306,14 @@ bool as_control(void){
    //if (k < 2 && j>2){k += 1;} // From 3rd mot status only activate the tail surfaces (k>2)
    if (single_act && !act_selected(k_conv, selected_act_idx, asel)){k += 1;} // if actuator not in the list and setting on then skip
    else{
-    if (j>1 && !act_selected(k_conv, tail_act_idx,2) ){k += 1;
+    if (j>2 && !act_selected(k_conv, tail_act_idx,2) ){k += 1;
     printf("Actuator not in tail \n");} // if performing a pusher test skip all actuators except tail
     else{
       if(done_as){
         printf("Actuator = %i \n",k_conv);
+        tcp =(float) ((k+1) + j * (kmax+1) + i * (jmax+1) * (kmax+1))/((imax+1)*(jmax+1)*(kmax+1))*100.;
+        printf("Test Completion Percentage %0.01f %% \n", tcp);
+        printf("Elapsed time %0.1f [s] \n",get_sys_time_float()-t_test);
         done_as = false;}
       else{
         if(!excitation_control()){
@@ -339,7 +350,8 @@ bool excitation_control(void){
      else{                                                  // Else calculate the cmd to the actuator
        ratio_excitation = stopwatch / dt_m / ramp_ratio;    // Calclate the percentage of completion of the initial linear sweep
        Bound(ratio_excitation, 0, 1);
-       if (ratio_excitation==1){verbose_test_ID=true;}
+       verbose_test_ID=true;
+      //  if (ratio_excitation==1){verbose_test_ID=true;}
        actuators_wt[k_conv] = (int16_t) cmd_0 + (cmd_target - cmd_0) * ratio_excitation;}}}
    return true;
    }else{                                                                       // If all excitation explored, initiate a shutoff sequence of the actuator
@@ -376,6 +388,13 @@ static void send_windtunnel_static(struct transport_tx *trans, struct link_devic
        point_id[0] = '\0';
        strcat(point_id, "Blank");}
   else{ID_gen();}
+
+  #if !USE_NPS
+    aoa_wt = aoa_pwm.angle;
+  #else
+    aoa_wt = 0;
+  #endif
+
   pprz_msg_send_WINDTUNNEL_STATIC(trans, dev, AC_ID,
                                         strlen(point_id), point_id,
                                         &test_active_conv,
@@ -392,6 +411,8 @@ static void send_windtunnel_static(struct transport_tx *trans, struct link_devic
                                         &o,
                                         &p2,
                                         &max_rotation_rate,
+                                        &tcp,
+                                        &aoa_wt,
                                         11, actuators_wt,
                                         11, actuators
                                         );
@@ -410,7 +431,7 @@ bool act_selected(int8_t val, int8_t sel_val[], int8_t size_sel){
   int8_t i_sel;
   for(i_sel = 0; i_sel < size_sel; i_sel++){
     if(sel_val[i_sel]==val){
-      printf("Integer %i is in array position %i\n",val,i_sel);
+      //printf("Integer %i is in array position %i\n",val,i_sel);
       return true;}}
   return false;}
 //#######################################################################################################################################################################################
