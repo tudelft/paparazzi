5886ef70054391285121f2c88caa286266e04d4e
Christophe De Wagter <dewagter@gmail.com>
2023-02-16 04:12:38 -0800

    [module] GNCNets Robin

---
+++ sw/airborne/modules/gcnet/ekf_gps.h
@@ -0,0 +1,36 @@
+/*
+Extened kalman filter based on https://en.wikipedia.org/wiki/Extended_Kalman_filter
+Section 5.3: Non-additive noise formulation and equations
+*/
+
+#ifndef EKF_GPS_H
+#define EKF_GPS_H
+
+#define EKF_NUM_STATES 15
+#define EKF_NUM_INPUTS 6
+#define EKF_NUM_OUTPUTS 4
+
+#include "math/pprz_algebra_float.h"
+
+
+extern float ekf_X[EKF_NUM_STATES];
+extern float ekf_P[EKF_NUM_STATES][EKF_NUM_STATES];
+extern float ekf_Q[EKF_NUM_INPUTS][EKF_NUM_INPUTS];
+extern float ekf_R[EKF_NUM_OUTPUTS][EKF_NUM_OUTPUTS];
+extern float ekf_H[EKF_NUM_OUTPUTS][EKF_NUM_STATES];
+
+void ekf_set_diag(float **a, float *b, int n);
+
+extern void ekf_init(void);
+
+void ekf_f(const float X[EKF_NUM_STATES], const float U[EKF_NUM_INPUTS], float out[EKF_NUM_STATES]);
+void ekf_F(const float X[EKF_NUM_STATES], const float U[EKF_NUM_INPUTS], float out[EKF_NUM_STATES][EKF_NUM_STATES]);
+void ekf_L(const float X[EKF_NUM_STATES], const float U[EKF_NUM_INPUTS], float out[EKF_NUM_STATES][EKF_NUM_INPUTS]);
+
+void ekf_f_rk4(const float X[EKF_NUM_STATES], const float U[EKF_NUM_INPUTS], const float dt, float out[EKF_NUM_STATES]);
+
+extern void ekf_step(const float U[EKF_NUM_INPUTS], const float Z[EKF_NUM_OUTPUTS], const float dt);
+extern void ekf_prediction_step(const float U[EKF_NUM_INPUTS], const float dt);
+extern void ekf_measurement_step(const float Z[EKF_NUM_OUTPUTS], const float dt);
+
+#endif
