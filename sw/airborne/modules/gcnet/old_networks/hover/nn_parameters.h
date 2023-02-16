5484461efe3208b0fcf46171689b28c3e73ddb1d
Christophe De Wagter <dewagter@gmail.com>
2023-02-16 04:12:38 -0800

    [module] GNCNets Robin

---
+++ sw/airborne/modules/gcnet/old_networks/hover/nn_parameters.h
@@ -0,0 +1,32 @@
+#ifndef NN_PARAMETERS_H
+#define NN_PARAMETERS_H
+
+#define NUM_STATES 19
+#define NUM_CONTROLS 4
+#define NUM_LAYERS 4
+#define NUM_HIDDEN_LAYERS 2
+#define NUM_NODES 120
+
+
+// NN network parameters -- define variables
+extern const float weights_in[NUM_NODES][NUM_STATES];
+
+extern const float bias_in[NUM_NODES];
+
+extern const float weights_out[NUM_CONTROLS][NUM_NODES];
+
+extern const float bias_out[NUM_CONTROLS];
+
+extern const float in_norm_mean[NUM_STATES];
+
+extern const float in_norm_std[NUM_STATES];
+
+extern const float out_scale_min;
+
+extern const float out_scale_max;
+
+extern const float weights_hid[NUM_HIDDEN_LAYERS-1][NUM_NODES][NUM_NODES];
+
+extern const float bias_hid[NUM_HIDDEN_LAYERS-1][NUM_NODES];
+
+#endif
