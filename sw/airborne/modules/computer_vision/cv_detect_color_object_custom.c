cd3afa68dca9d2506f32c16ad696ffa4d2c1c908
rovwtr <85065423+rovwtr@users.noreply.github.com>
2023-03-16 14:12:20 +0100

    Update cv_detect_color_object_custom.c

--- sw/airborne/modules/computer_vision/cv_detect_color_object_custom.c
+++ sw/airborne/modules/computer_vision/cv_detect_color_object_custom.c
@@ -74,6 +74,12 @@ bool cod_draw2 = false;
 struct color_object_t {
   int32_t x_c;
   int32_t y_c;
+  
+  //new
+  int32_t vector_x;
+  int32_t vector_y;
+  //new
+  
   uint32_t color_count;
   bool updated;
 };
@@ -124,7 +130,7 @@ static struct image_t *object_detector(struct image_t *img, uint8_t filter)
   int32_t x_c, y_c;
 
   // Filter and find centroid
-  uint32_t count = find_object_centroid(img, &x_c, &y_c, draw, lum_min, lum_max, cb_min, cb_max, cr_min, cr_max);
+  uint32_t count, vx, vy = find_object_centroid(img, &x_c, &y_c, draw, lum_min, lum_max, cb_min, cb_max, cr_min, cr_max);
   VERBOSE_PRINT("Color count %d: %u, threshold %u, x_c %d, y_c %d\n", camera, object_count, count_threshold, x_c, y_c);
   VERBOSE_PRINT("centroid %d: (%d, %d) r: %4.2f a: %4.2f\n", camera, x_c, y_c,
         hypotf(x_c, y_c) / hypotf(img->w * 0.5, img->h * 0.5), RadOfDeg(atan2f(y_c, x_c)));
@@ -134,6 +140,12 @@ static struct image_t *object_detector(struct image_t *img, uint8_t filter)
   global_filters[filter-1].x_c = x_c;
   global_filters[filter-1].y_c = y_c;
   global_filters[filter-1].updated = true;
+  
+  //new
+  global_filters[filter-1].vector_x = vx;
+  global_filters[filter-1].vector_y = vy;
+  //new
+  
   pthread_mutex_unlock(&mutex);
 
   return img;
@@ -215,45 +227,145 @@ uint32_t find_object_centroid(struct image_t *img, int32_t* p_xc, int32_t* p_yc,
   uint32_t tot_x = 0;
   uint32_t tot_y = 0;
   uint8_t *buffer = img->buf;
+  
+  int8_t kernel_size = 25;
+  int16_t heigth = img->h;
+  int16_t width = img->w;
+  int16_t kernel_cnt = 0;
 
-  // Go through all the pixels
-  for (uint16_t y = 0; y < img->h; y++) {
-    for (uint16_t x = 0; x < img->w; x ++) {
-      // Check if the color is inside the specified values
-      uint8_t *yp, *up, *vp;
-      if (x % 2 == 0) {
-        // Even x
-        up = &buffer[y * 2 * img->w + 2 * x];      // U
-        yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y1
-        vp = &buffer[y * 2 * img->w + 2 * x + 2];  // V
-        //yp = &buffer[y * 2 * img->w + 2 * x + 3]; // Y2
-      } else {
-        // Uneven x
-        up = &buffer[y * 2 * img->w + 2 * x - 2];  // U
-        //yp = &buffer[y * 2 * img->w + 2 * x - 1]; // Y1
-        vp = &buffer[y * 2 * img->w + 2 * x];      // V
-        yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y2
-      }
-      if ( (*yp >= lum_min) && (*yp <= lum_max) &&
+  int8_t sensitivity = 170;
+  int8_t lower_black = 41;
+  int8_t upper_white = 183;
+  int16_t threshold = 255;
+
+  int16_t x = 0;
+  int16_t y = 0;
+  int16_t kur_x = 0;
+  int16_t kur_y = 0;
+
+  int16_t kernel_centroid = 0;
+
+  int16_t kernel_w_cnt = floor(width/kernel_size);
+  int16_t kernel_h_cnt = floor(heigth/kernel_size);
+
+  int16_t vector_array[20] = {0};
+
+  for (int8_t y_k = 0; y_k < kernel_h_cnt; y_k++){
+    // for (int8_t x_k = 0; x_k < kernel_w_cnt; x_k++){
+
+      int8_t state = 0;
+
+      for (int8_t x_k = kernel_w_cnt-1; x_k >= 0; x_k--){
+
+      kernel_cnt = 0;
+      kur_x = kernel_size*x_k;
+      kur_y = kernel_size*y_k;
+      for (int8_t i = 0; i < kernel_size; i++){
+        for (int8_t j = 0; j < kernel_size; j++){
+          x = kur_x + j;
+          y = kur_y + i;
+
+          uint8_t *yp, *up, *vp;
+          if (x % 2 == 0) {
+            // Even x
+            up = &buffer[y * 2 * img->w + 2 * x];      // U
+            yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y1
+            vp = &buffer[y * 2 * img->w + 2 * x + 2];  // V
+            //yp = &buffer[y * 2 * img->w + 2 * x + 3]; // Y2
+          } else {
+            // Uneven x
+            up = &buffer[y * 2 * img->w + 2 * x - 2];  // U
+            //yp = &buffer[y * 2 * img->w + 2 * x - 1]; // Y1
+            vp = &buffer[y * 2 * img->w + 2 * x];      // V
+            yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y2
+          }
+
+          // if ((*up < sensitivity) && (*vp < sensitivity) && (*yp > lower_black) && (*yp < upper_white)){
+          if ( (*yp >= lum_min) && (*yp <= lum_max) &&
            (*up >= cb_min ) && (*up <= cb_max ) &&
            (*vp >= cr_min ) && (*vp <= cr_max )) {
-        cnt ++;
-        tot_x += x;
-        tot_y += y;
-        if (draw){
-          *yp = 255;  // make pixel brighter in image
+            if (draw){
+              *yp = 255;  // make pixel brighter in image
+            }
+            kernel_cnt++;
+          }          
         }
       }
+      //add break when ready
+      //TODO
+      if (kernel_cnt > threshold){
+        if (state == 0) {
+          state == 1;
+          kernel_centroid = kernel_size * x_k + floor(kernel_size/2);
+          vector_array[y_k] = kernel_centroid;
+        }
+      }
+      else {
+        if (state == 0) {
+          state == 1;
+          kernel_centroid = 0;
+          vector_array[y_k] = kernel_centroid;
+        }
+
+      }
+    }
+    if (draw){
+      int vector_count = 0;
+      for (int y = floor(kernel_size/2); y < img->h; y+= floor(kernel_size/2)){
+        int max = vector_array[vector_count];
+        for(int x = 0; x < max; x++){
+          uint8_t *yp, *up, *vp;
+          if (x % 2 == 0) {
+            // Even x
+            up = &buffer[y * 2 * img->w + 2 * x];      // U
+            yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y1
+            vp = &buffer[y * 2 * img->w + 2 * x + 2];  // V
+            //yp = &buffer[y * 2 * img->w + 2 * x + 3]; // Y2
+          } else {
+            // Uneven x
+            up = &buffer[y * 2 * img->w + 2 * x - 2];  // U
+            //yp = &buffer[y * 2 * img->w + 2 * x - 1]; // Y1
+            vp = &buffer[y * 2 * img->w + 2 * x];      // V
+            yp = &buffer[y * 2 * img->w + 2 * x + 1];  // Y2
+          }
+          *up = 0;
+          *vp = 255;
+          *yp = 125;
+        }
+      vector_count++;
+
+      }
+    }
+    //add 0,0 to list
+    // kernel_centroid = 0;
+    // vector_array[y_k] = kernel_centroid;
+  }
+
+  //DEBUG LOOP
+
+
+
+  cnt = 1;
+  // PRINT("CNT IN CV_OBS = %d", cnt);
+
+  int32_t vector_x = 4;
+  int32_t vector_y = 20;
+
+  // PRINT("Vector x IN CV_OBS = %d", vector_x);
+  // PRINT("Vector y IN CV_OBS = %d", vector_y);
+
+  cnt = vector_array[10];
+  int32_t length = 0;
+  int32_t pos = 0;
+  for (int8_t i = 0; i < 20; i++){
+    int8_t pot_max = vector_array[i];
+    if (pot_max > length){
+      length = pot_max;
+      pos = i;
     }
   }
-  if (cnt > 0) {
-    *p_xc = (int32_t)roundf(tot_x / ((float) cnt) - img->w * 0.5f);
-    *p_yc = (int32_t)roundf(img->h * 0.5f - tot_y / ((float) cnt));
-  } else {
-    *p_xc = 0;
-    *p_yc = 0;
-  }
-  return cnt;
+  vector_y = (pos * kernel_size) + floor(kernel_size/2);
+  return cnt, length, vector_y;
 }
 
 void color_object_detector_periodic(void)
@@ -265,12 +377,12 @@ void color_object_detector_periodic(void)
 
   if(local_filters[0].updated){
     AbiSendMsgVISUAL_DETECTION(COLOR_OBJECT_DETECTION1_ID, local_filters[0].x_c, local_filters[0].y_c,
-        0, 0, local_filters[0].color_count, 0);
+        local_filters[0].vector_x, local_filters[0].vector_y, local_filters[0].color_count, 0);
     local_filters[0].updated = false;
   }
   if(local_filters[1].updated){
     AbiSendMsgVISUAL_DETECTION(COLOR_OBJECT_DETECTION2_ID, local_filters[1].x_c, local_filters[1].y_c,
-        0, 0, local_filters[1].color_count, 1);
+        local_filters[1].vector_x, local_filters[1].vector_y, local_filters[1].color_count, 1);
     local_filters[1].updated = false;
   }
 }
