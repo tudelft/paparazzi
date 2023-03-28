0832fba7506ed356c32aa58396b3c1893b1fbc31
bernardohenriques <bernardo.henriques7@gmail.com>
2023-03-28 19:58:37 +0200

    changes

--- sw/airborne/modules/bebop2_bernardo_module/bebop2_bernardo.h
+++ sw/airborne/modules/bebop2_bernardo_module/bebop2_bernardo.h
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2021 Matteo Barbera <matteo.barbera97@gmail.com>
- *
- * This file is part of Paparazzi.
- *
- * Paparazzi is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * Paparazzi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Paparazzi; see the file COPYING.  If not, see
- * <http://www.gnu.org/licenses/>.
- */
-
 #ifndef BEBOP2_BERNARDO_H
 #define BEBOP2_BERNARDO_H
 
