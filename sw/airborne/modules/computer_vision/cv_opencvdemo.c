f95341a0d3f641cecc92d780dd6365b53e3cbdbd
mpvanandel <mpvanandel@gmail.com>
2023-04-03 15:35:11 +0200

    clean cv_opencvdemo

--- sw/airborne/modules/computer_vision/cv_opencvdemo.c
+++ sw/airborne/modules/computer_vision/cv_opencvdemo.c
@@ -26,6 +26,7 @@
 #include "modules/computer_vision/cv.h"
 #include "modules/computer_vision/cv_opencvdemo.h"
 #include "modules/computer_vision/opencv_example.h"
+#include "modules/computer_vision/finite_state_machine.h"
 
 
 #include "modules/orange_avoider/orange_avoider.h"
@@ -61,14 +62,14 @@
 #endif
 #define WIDTH_2_PROCESS 30   // TWEAKABLE
 #define HEIGHT_2_PROCESS 140 // TWEAKABLE
-#define FLOWMIDDLE_DIV_LOWER 0.80f
-#define FLOWMIDDLE_ABS_MIN 0.0f // Not too sure whether using this is a smart move, otherwise set to 0
-#define LEFTFLOW_TURNING_TH 1.22f
-#define RIGHTFLOW_TURNING_TH 0.75f
-#define COUNTER2_MAX 9
-#define HEADING_INC_LEFT_RIGHT 45.f
-#define HEADING_INC_MIDDLE 60.f
-#define HEADING_INC_OUT_OF_BOUND 90.f
+// #define FLOWMIDDLE_DIV_LOWER 0.80f
+// #define FLOWMIDDLE_ABS_MIN 0.0f // Not too sure whether using this is a smart move, otherwise set to 0
+// #define LEFTFLOW_TURNING_TH 1.22f
+// #define RIGHTFLOW_TURNING_TH 0.75f
+// #define COUNTER2_MAX 9
+// #define HEADING_INC_LEFT_RIGHT 45.f
+// #define HEADING_INC_MIDDLE 60.f
+// #define HEADING_INC_OUT_OF_BOUND 90.f
 
 float movedistance = 1.2f;     // TWEAKABLE (changes spead)
 
@@ -83,46 +84,26 @@ float y_prev;
 float tot_dist = 0.f;
 
 
-// ::google::protobuf::internal::GetCurrentTime(&seconds, &nanoseconds);
-// int64_t seconds;
-// int32_t nanoseconds;
+// // ::google::protobuf::internal::GetCurrentTime(&seconds, &nanoseconds);
+// // int64_t seconds;
+// // int32_t nanoseconds;
 #define LOG(x) fprintf(stderr, "LOG: %s:%d %s %lu \n", __FILE__, __LINE__, x, clock());
-// #define LOG(x)
+// // #define LOG(x)
 
 
 ///////////////////////////////////////////////////////////////////
 
 
-static uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters);
-static uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters);
-static uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor);
-static uint8_t increase_nav_heading(float incrementDegrees);
+// static uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters);
+// static uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters);
+// static uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor);
+// static uint8_t increase_nav_heading(float incrementDegrees);
 
 float oa_color_count_frac = 0.18f; // ***HAVE TO DEFINE THIS HERE TO GET IT TO COMPILE
 
 // Define navigation states
-enum navigation_state_t {
-  SAFE,
-  IJUSTTURNED1,
-  IJUSTTURNED2,
-  OBSTACLE_LEFT,
-  OBSTACLE_RIGHT,
-  OBSTACLE_MIDDLE,
-  OUT_OF_BOUNDS,
-};
 
-enum navigation_state_t navigation_state = SAFE;
-float flowleft = 0.0f;
-float flowright = 0.0f;
-float flowmiddle = 1.0f;
-float flowmiddle_prev = 1.0f;
-float flowmiddle_prev_prev = 1.0f;
-float flowmiddle_divergence = 1.0f;
-float flowmiddle_divergence_prev = 1.0f;
 
-// float flowleft_threshold = 5.0f;
-// float flowright_threshold = 5.0f;
-// float flowmiddle_threshold = 5.0f;
 
 float right_left_normalizer = 1.0f;                                  // TWEAKABLE
 // float right_obstacle_threshold = 1.0f // TWEAKABLE
@@ -135,22 +116,7 @@ float flowright_temp = 0.0f;
 int counter = 0;
 int counter2 = 0;
 int step = 0;
-// float flowcombined_treshold = 10.0f;
-// if flowcombined > flowcombined_treshold, then turn 180 degrees (run away)
-
-// float heading_increment = 180.f; // TWEAKABLE (triggered when out of bounds)
 float output_flow[3];
-
-// void yaw_cancel(struct image_t *img, float flow_left, float flow_right, float flow_middle_divergence) {
-//     if (img->eulers.psi < 0.3)
-//     {
-//         flow_left = 1;
-//         flow_right = 1;
-//         flow_middle_divergence = 1;
-
-//     }
-// }
-
 
 
 
@@ -250,224 +216,16 @@ void image_cover(struct image_t *img){
     }
 }
 
-
-
-
-
 struct image_t *optical_flow_func(struct image_t *img, int camera_id);
-
-
 struct image_t *optical_flow_func(struct image_t *img, int camera_id)
 {
-    // action act = STANDBY;
-    // LOG("before farneback")
     if (img->type == IMAGE_YUV422) {
         farneback((char *) img->buf, output_flow, WIDTH_2_PROCESS, HEIGHT_2_PROCESS, img->w, img->h);
-       
-    }
-    compute_dist(step);
-    step = 1;
-    LOG("after farneback")
-    // yaw_cancel(img, output_flow[0], output_flow[1], flowmiddle_divergence);
-
-    //increase_nav_heading(6.f);
-    //moveWaypointForward(WP_TRAJECTORY, movedistance);
-    //moveWaypointForward(WP_GOAL, 0.5);
-    //
-    flowmiddle_prev_prev = flowmiddle_prev;
-    flowmiddle_prev = flowmiddle;
-    flowleft = output_flow[0];
-    flowright = output_flow[1];
-    flowmiddle = output_flow[2];
-
-    flowmiddle_divergence = (flowmiddle / flowmiddle_prev);
-    flowmiddle_divergence_prev = (flowmiddle / flowmiddle_prev_prev);
-    right_left_normalizer = flowleft / flowright; // ADDED THIS, ABSULUTE VALUES DONT SEEM TO WORK SO WELL
-
-
-    printf("New cycle \n");
-    printf("flowleft: %f \n", flowleft);
-    printf("flowright: %f \n", flowright);
-    printf("flowmiddle: %f \n", flowmiddle);
-    printf("flowmiddle_prev: %f \n", flowmiddle_prev);
-    printf("right_left_normalizer: %f \n", right_left_normalizer);
-    printf("flowmiddle divergence: %f \n", flowmiddle_divergence);
-    printf("flowmiddle_divergence_prev: %f \n", flowmiddle_divergence_prev);
-    printf("Navigation state: %d \n", navigation_state);
-
-
- 
-    if (right_left_normalizer < right_obstacle_threshold|| right_left_normalizer > left_obstacle_threshold|| flowmiddle_divergence > 1.5){
-        image_editing(img,right_left_normalizer,flowmiddle_divergence);
-    }
-    else{
-//        printf("No flow above threshold \n");
     }
 
-    image_cover(img);
-    printf("Yaw: %f", img->eulers.psi);
+    run_fsm(output_flow[0], output_flow[1], output_flow[2]);
 
-
-    switch (navigation_state){
-      case SAFE:
-        // Move waypoint forward
-        moveWaypointForward(WP_TRAJECTORY, 1.0f * movedistance);
-
-        LOG("start of safe")
-        
-     
-        if (!InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))){
-          navigation_state = OUT_OF_BOUNDS;
-          printf("OUT OF BOUNDS \n");
-          break; 
-          
-  
-        } else if (right_left_normalizer < right_obstacle_threshold){   // TWEAK EFFECT HERE
-          // LOG("after obstacle right evaluation")
-          printf("Obstacle RIGHT \n");
-          navigation_state = OBSTACLE_RIGHT;
-          break; 
-          
-        } else if (right_left_normalizer > left_obstacle_threshold){ //  TWEAK EFFECT HERE
-          printf("Obstacle LEFT \n");
-          navigation_state = OBSTACLE_LEFT;
-          break; 
-          
-        } 
-          else if (flowmiddle_divergence > flowmiddle_obstacle_threshold){  // TWEAK EFFECT HERE
-          printf("Obstacle MIDDLE \n");
-          navigation_state = OBSTACLE_MIDDLE;break;
-          break; 
-          
-        } else if (flowmiddle_divergence < FLOWMIDDLE_DIV_LOWER){    // TWEAKABLE
-          printf("Obstacle MIDDLE \n");
-          navigation_state = OBSTACLE_MIDDLE;
-          break; 
-        } 
-          else if (flowmiddle_divergence_prev < 0.45){    // TWEAKABLE
-          printf("Obstacle MIDDLE \n");
-          navigation_state = OBSTACLE_MIDDLE;
-          break; 
-        } 
-        else if (flowmiddle_divergence_prev > 2.0f){    // TWEAKABLE
-          printf("Obstacle MIDDLE \n");
-          navigation_state = OBSTACLE_MIDDLE;
-          break; 
-        } 
-
-        else if (flowmiddle  < FLOWMIDDLE_ABS_MIN){    // TWEAKABLE
-          printf("Obstacle MIDDLE \n");
-          increase_nav_heading(90.0f);
-          navigation_state = SAFE;
-          break; 
-        } 
-        
-        else {
-          moveWaypointForward(WP_GOAL, 0.5f * movedistance);
-          printf("NO OBSTACLE \n");
-        }
-        LOG("end of safe")
-
-        break;
-
-      case IJUSTTURNED1: // Stands still for a little bit 
-      moveWaypointForward(WP_TRAJECTORY, 0.5 * movedistance);
-      moveWaypointForward(WP_GOAL, 0.5* movedistance);
-      LOG("IJUSTTURNED1")
-      counter++;
-      printf("counter: %d \n", counter);
-
-      if (counter == 3){  // TWEAKABLE
-        navigation_state = IJUSTTURNED2;
-        counter = 0;
-      }
-
-      break;
-
-      case IJUSTTURNED2:
-      LOG("IJUSTTURNED2")
-      moveWaypointForward(WP_TRAJECTORY, 0.5 * movedistance);
-      moveWaypointForward(WP_GOAL, 0.5* movedistance);
-      counter2++;
-
-      if (!InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))){
-        navigation_state = OUT_OF_BOUNDS;
-        counter2 = 0;
-        break;
-        
-  
-        } else if (right_left_normalizer < RIGHTFLOW_TURNING_TH){ // added this
-          LOG("after obstacle right evaluation")
-          printf("Obstacle RIGHT \n");
-          navigation_state = OBSTACLE_RIGHT;
-          counter2 = 0;
-          break;
-         
-        } else if (right_left_normalizer > LEFTFLOW_TURNING_TH){ 
-          printf("Obstacle LEFT \n");
-          navigation_state = OBSTACLE_LEFT;
-          counter2 = 0;
-          break;
-        } 
-
-     
-      printf("counter2: %d \n", counter2);
-      if (counter2 == COUNTER2_MAX){
-        navigation_state = SAFE;
-        counter2 = 0;
-        break;
-      }
-      
-      break;
-
-
-      case OBSTACLE_LEFT:
-        LOG("OBSTACLE LEFT")
-        waypoint_move_here_2d(WP_GOAL);
-        waypoint_move_here_2d(WP_TRAJECTORY);
-        increase_nav_heading(HEADING_INC_LEFT_RIGHT); // SHOULD BE TWEAKED
-        printf("yaw left %f : \n ", img->eulers.psi);
-        navigation_state = IJUSTTURNED1;
-        break;
-
-      case OBSTACLE_RIGHT:
-        LOG("OBSTACLE RIGHT")
-        waypoint_move_here_2d(WP_GOAL);
-        waypoint_move_here_2d(WP_TRAJECTORY);
-        increase_nav_heading(-HEADING_INC_LEFT_RIGHT); // SHOULD BE TWEAKED
-        printf("yaw left %f : \n ", img->eulers.psi);
-        navigation_state = IJUSTTURNED1;
-        break;
-
-    case OBSTACLE_MIDDLE: 
-      LOG("OBSTACLE MIDDLE")
-      waypoint_move_here_2d(WP_GOAL);
-      waypoint_move_here_2d(WP_TRAJECTORY);
-      increase_nav_heading(HEADING_INC_MIDDLE);
-      navigation_state = IJUSTTURNED1; 
-      break;
-    case OUT_OF_BOUNDS:
-      LOG("Out of bounds")
-      waypoint_move_here_2d(WP_GOAL);
-      waypoint_move_here_2d(WP_TRAJECTORY);
-      increase_nav_heading(HEADING_INC_OUT_OF_BOUND);
-      moveWaypointForward(WP_TRAJECTORY, 1.0f * movedistance);
-      if (!InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))){
-        navigation_state = OUT_OF_BOUNDS; 
-        break;
-        }
-      else {
-        LOG("NOT OUT OF BOUNDS ANYMORE")
-        moveWaypointForward(WP_GOAL, 1.0f * movedistance);
-        navigation_state = IJUSTTURNED2;
-        break;
-      }
-      break;
-    default:
-      break;
-  }
-
-     return img;
+    return img;
 }
 
 void calc_action_optical_flow_init(void)
@@ -477,76 +235,6 @@ void calc_action_optical_flow_init(void)
     output_flow[1] = 0.0;
 }
 
-
-
-
-
-
-// moveWaypointForward(WP_TRAJECTORY, movedistance);
-
-void calc_action_optical_flow_periodic(void)
-{
-//    printf("out: %f",output_flow[0]);
-
-}
-// define variables
-////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
- * Increases the NAV heading. Assumes heading is an INT32_ANGLE. It is bound in this function.
- */
-uint8_t increase_nav_heading(float incrementDegrees)
-{
-  float new_heading = stateGetNedToBodyEulers_f()->psi + RadOfDeg(incrementDegrees);
-
-  // normalize heading to [-pi, pi]
-  FLOAT_ANGLE_NORMALIZE(new_heading);
-
-  // set heading, declared in firmwares/rotorcraft/navigation.h
-  // for performance reasons the navigation variables are stored and processed in Binary Fixed-Point format
-  nav_heading = ANGLE_BFP_OF_REAL(new_heading);
-
-//  VERBOSE_PRINT("Increasing heading to %f\n", DegOfRad(new_heading));
-
-  return false;
-}
-
-/*
- * Calculates coordinates of distance forward and sets waypoint 'waypoint' to those coordinates
- */
-uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters)
-{
-  struct EnuCoor_i new_coor;
-  calculateForwards(&new_coor, distanceMeters);
-  moveWaypoint(waypoint, &new_coor);
-  return false;
-}
-
-/*
- * Calculates coordinates of a distance of 'distanceMeters' forward w.r.t. current position and heading
- */
-uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters)
-{
-  float heading  = stateGetNedToBodyEulers_f()->psi;
-
-  // Now determine where to place the waypoint you want to go to
-  new_coor->x = stateGetPositionEnu_i()->x + POS_BFP_OF_REAL(sinf(heading) * (distanceMeters));
-  new_coor->y = stateGetPositionEnu_i()->y + POS_BFP_OF_REAL(cosf(heading) * (distanceMeters));
-//  VERBOSE_PRINT("Calculated %f m forward position. x: %f  y: %f based on pos(%f, %f) and heading(%f)\n", distanceMeters,
-//                POS_FLOAT_OF_BFP(new_coor->x), POS_FLOAT_OF_BFP(new_coor->y),
-//                stateGetPositionEnu_f()->x, stateGetPositionEnu_f()->y, DegOfRad(heading));
-  return false;
-}
-
-
-
-uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor)
-{
-//  VERBOSE_PRINT("Moving waypoint %d to x:%f y:%f\n", waypoint, POS_FLOAT_OF_BFP(new_coor->x),
-//                POS_FLOAT_OF_BFP(new_coor->y));
-  waypoint_move_xy_i(waypoint, new_coor->x, new_coor->y);
-  return false;
-}
 
 void compute_dist(int i)
 {
--- sw/airborne/modules/computer_vision/cv_opencvdemo.h
+++ sw/airborne/modules/computer_vision/cv_opencvdemo.h
@@ -27,12 +27,7 @@
 #define CV_OPENCVDEMO_H
 
 // // extern void opencvdemo_init(void);
-// // struct FlowData
-// // {
-// //   left;
-// //   right;
-// //   middle
-// // }
+
 // void finite_state_machine(FlowData flow_data);
 extern void calc_action_optical_flow_init(void);
 extern void calc_action_optical_flow_periodic(void);
