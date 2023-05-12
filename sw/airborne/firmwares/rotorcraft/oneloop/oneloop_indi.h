12520f7ff32992f4b2020997eac13e3ac13a1508
TML De Ponti <tomasodp@gmail.com>
2023-05-12 18:56:59 +0200

    started one loop controller

---
+++ sw/airborne/firmwares/rotorcraft/oneloop/oneloop_indi.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2023 Tomaso De Ponti <tmldeponti@tudelft.nl>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/** @file "modules/wind_tunnel/oneloop_indi.h"
+ * @author Tomaso De Ponti <tmldeponti@tudelft.nl>
+ * One loop (Guidance + Stabilization) INDI controller for the rotating wing drone RW3C
+ */
+
+#ifndef ONELOOP_INDI_H
+#define ONELOOP_INDI_H
+
+
+#endif  // ONELOOP_INDI_H
