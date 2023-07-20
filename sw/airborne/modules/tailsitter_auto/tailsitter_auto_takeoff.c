64e275111defae923c8e1ba32e4d3bd49dc847a6
Ewoud Smeur <e.j.j.smeur@tudelft.nl>
2023-07-20 12:12:11 +0200

    cleanup code, untested

--- sw/airborne/modules/tailsitter_auto/tailsitter_auto_takeoff.c
+++ sw/airborne/modules/tailsitter_auto/tailsitter_auto_takeoff.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2023 Yamac Birol
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include "generated/airframe.h"
 #include "state.h"
 #include <stdio.h>
@@ -7,12 +28,9 @@
 #include "firmwares/rotorcraft/autopilot_static.h"
 #include "autopilot.h"
 
-#define PRINT(string, ...) fprintf(stderr, "[mav_exercise->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
-
 int16_t stage = 0;
 int16_t counter = 0;
 float Kq = TAKEOFF_Q_GAIN;
-int16_t stage_1_thrust = 6400;
 
 int16_t pwm2pprz(float pwm);
 int16_t take_off_stage(float theta);
@@ -22,7 +40,6 @@ void take_off_enter(void);
 void take_off_enter(void){
   stage = 0;
   counter = 0;
-  autopilot_static_set_motors_on(true);
 }
 
 int16_t take_off_thrust(void){
@@ -32,53 +49,49 @@ int16_t take_off_thrust(void){
   struct FloatQuat * statequat = stateGetNedToBodyQuat_f();
   struct FloatRates * body_rates = stateGetBodyRates_f();
   float_eulers_of_quat_zxy(&eulers_zxy, statequat);
-  //int temp_stage = take_off_stage(eulers_zxy.theta);
+
   if(autopilot.mode == AP_MODE_NAV){
     if(stage == 0){
       thrust_pwm = 1000;
       thrust_pprz = pwm2pprz(thrust_pwm);
-    }
-    else if(stage == 1){
-      //thrust_pwm = 105.1233*eulers_zxy.theta + 1764.7072;
-      //thrust_pprz = pwm2pprz(thrust_pwm);
-      int16_t pitch_rate_cont = (int16_t)ceil(Kq*(0.3 - body_rates->q));
-      thrust_pprz = stage_1_thrust - pitch_rate_cont;
+    } else if(stage == 1){
+      int16_t pitch_rate_cont = (int16_t) ceil(Kq*(0.3 - body_rates->q));
+      thrust_pprz = 6400 - pitch_rate_cont;
     }
     else if(stage == 2){
       thrust_pwm = -22.9362*eulers_zxy.theta + 1697.9914;
       thrust_pprz = pwm2pprz(thrust_pwm);
+    } else{
+      thrust_pwm = 1542;
+      thrust_pprz = pwm2pprz(thrust_pwm);
     }
-    else{
-    thrust_pwm = 1542;
-    thrust_pprz = pwm2pprz(thrust_pwm);
-  }
   }
   else{
     thrust_pprz = radio_control.values[RADIO_THROTTLE];
   }
   return thrust_pprz;
-
 }
 
 int16_t take_off_stage(float theta){
-  //if((*stg != 0 || *stg != 3) && fabsf(theta) > RadOfDeg(85)){
-    //stg = 0;
- //} 
   counter++;
   if(autopilot.mode == AP_MODE_NAV){
-    if(stage == 0 && counter/500 > 1.0 && autopilot_get_motors_on()==true){
+    if (!autopilot_get_motors_on()) {
+      stage = 0;
+      counter = 0;
+    }
+    if(stage == 0 && counter/500 > 1.0){
       stage = 1;
     }
     else if(stage == 1 && fabsf(theta) < RadOfDeg(30.0)){
-      stage++; 
+      stage = 2;
     }
     else if(stage == 2 && fabsf(theta) < RadOfDeg(5.0)){
-      stage++;
+      stage = 3;
     }
     else if(stage == 3){
       counter = 0;
     }
-  } else{
+  } else {
     if(radio_control.values[RADIO_PIVOT_SWITCH] < -4500 ){
       counter = 0;
       stage = 0;
--- sw/airborne/modules/tailsitter_auto/tailsitter_auto_toff.h
+++
@@ -1,4 +0,0 @@
-#ifndef TAILSITTER_AUTO_TOFF_H
-#define TAILSITTER_AUTO_TOFF_H
-
-extern float take_off_thrust(void);
