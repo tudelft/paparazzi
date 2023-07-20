f168c5bdda49ca520e5347d2b0906d1cb6e21378
Ewoud Smeur <e.j.j.smeur@tudelft.nl>
2023-07-20 12:12:11 +0200

    cleanup code, untested

--- sw/airborne/modules/tailsitter_auto/tailsitter_auto_takeoff.h
+++ sw/airborne/modules/tailsitter_auto/tailsitter_auto_takeoff.h
@@ -1,11 +1,31 @@
+/*
+ * Copyright (C) 2023 Yamac Birol
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #ifndef TAILSITTER_AUTO_TOFF_H
 #define TAILSITTER_AUTO_TOFF_H
 
-extern int16_t pwm2pprz(float pwm);
 extern int16_t take_off_stage(float theta);
 extern int16_t take_off_thrust(void);
 extern void take_off_enter(void);
 
 extern float Kq;
-extern int16_t stage_1_thrust;
-#endif
+
+#endif // TAILSITTER_AUTO_TOFF_H
