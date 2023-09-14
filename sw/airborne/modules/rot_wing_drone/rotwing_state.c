29af4917217abb8e644bbf70ae5b4883ab5f5c03
Dennis van Wijngaarden <32736330+Dennis-Wijngaarden@users.noreply.github.com>
2023-09-14 10:17:13 +0200

    [rotwing_state] set correct (new) location of .h file

--- sw/airborne/modules/rot_wing_drone/rotwing_state.c
+++ sw/airborne/modules/rot_wing_drone/rotwing_state.c
@@ -23,7 +23,7 @@
  * This module keeps track of the current state of a rotating wing drone and desired state set by the RC or flightplan. Paramters are being scheduled in each change of a current state and desired state. Functions are defined in this module to call the actual state and desired state and set a desired state.
  */
 
-#include "modules/rotwing/rotwing_state.h"
+#include "modules/rot_wing_drone/rotwing_state.h"
 #include "firmwares/rotorcraft/guidance/guidance_indi_rot_wing.h"
 
 struct RotwingState rotwing_state;
