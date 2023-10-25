dd44e269f8eb65a95792accdfae5229d3f9ab1d6
alessandro-mancinelli <alexm95@hotmail.it>
2023-10-26 00:41:47 +0800

    added nonlinear CA folder

---
+++ sw/ext/Nonlinear_CA/MATLAB_generated_files/Nonlinear_CA_control_rf_w_ail_singular_tilt_constrains_types.h
@@ -0,0 +1,23 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ * File: Nonlinear_CA_control_rf_w_ail_singular_tilt_constrains_types.h
+ *
+ * MATLAB Coder version            : 5.4
+ * C/C++ source code generated on  : 02-Jun-2023 12:17:03
+ */
+
+#ifndef NONLINEAR_CA_CONTROL_RF_W_AIL_SINGULAR_TILT_CONSTRAINS_TYPES_H
+#define NONLINEAR_CA_CONTROL_RF_W_AIL_SINGULAR_TILT_CONSTRAINS_TYPES_H
+
+/* Include Files */
+#include "rtwtypes.h"
+
+#endif
+/*
+ * File trailer for
+ * Nonlinear_CA_control_rf_w_ail_singular_tilt_constrains_types.h
+ *
+ * [EOF]
+ */
