e1a2d4b28fceaef2f5f2a1c1c0d09168fb28917f
ziqingma <ziqing.ma9612@gmail.com>
2023-11-01 11:35:17 +0100

    fix zero effectiveness error

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
@@ -772,6 +772,7 @@ void guidance_indi_propagate_filters(void) {
  * @param a_diff acceleration errors in earth frame
  * @param body_v 3D vector to write the control objective v
  */
+<<<<<<< HEAD
 #if GUIDANCE_INDI_HYBRID_USE_WLS
 void WEAK guidance_indi_calcg_wing(float Gmat[GUIDANCE_INDI_HYBRID_V][GUIDANCE_INDI_HYBRID_U], struct FloatVect3 a_diff, float v_gih[GUIDANCE_INDI_HYBRID_V]) {
 
@@ -808,6 +809,9 @@ void WEAK guidance_indi_calcg_wing(float Gmat[GUIDANCE_INDI_HYBRID_V][GUIDANCE_I
   v_gih[2] = a_diff.z;
 }
 #elif defined(GUIDANCE_INDI_QUADPLANE)
+=======
+#if defined(GUIDANCE_INDI_QUADPLANE)
+>>>>>>> fix bug that gave zero effectiveness matrix (#3151)
 /**
  * Perform WLS
  *
@@ -855,11 +859,45 @@ void WEAK guidance_indi_calcg_wing(float Gmat[GUIDANCE_INDI_HYBRID_V][GUIDANCE_I
   body_v[1] = -spsi * a_diff.x + cpsi * a_diff.y;
   body_v[2] =  a_diff.z;
 }
-
 #else
+void WEAK guidance_indi_calcg_wing(float Gmat[GUIDANCE_INDI_HYBRID_V][GUIDANCE_INDI_HYBRID_U], struct FloatVect3 a_diff, float v_gih[GUIDANCE_INDI_HYBRID_V]) {
 
-void WEAK guidance_indi_calcg_wing(float Gmat[GUIDANCE_INDI_HYBRID_V][GUIDANCE_INDI_HYBRID_U] UNUSED, struct FloatVect3 a_diff UNUSED, float body_v[GUIDANCE_INDI_HYBRID_V] UNUSED) {}
+  /*Pre-calculate sines and cosines*/
+  float sphi = sinf(eulers_zxy.phi);
+  float cphi = cosf(eulers_zxy.phi);
+  float stheta = sinf(eulers_zxy.theta);
+  float ctheta = cosf(eulers_zxy.theta);
+  float spsi = sinf(eulers_zxy.psi);
+  float cpsi = cosf(eulers_zxy.psi);
+  //minus gravity is a guesstimate of the thrust force, thrust measurement would be better
 
+#ifndef GUIDANCE_INDI_PITCH_EFF_SCALING
+#define GUIDANCE_INDI_PITCH_EFF_SCALING 1.0
+#endif
+
+  /*Amount of lift produced by the wing*/
+  float pitch_lift = eulers_zxy.theta;
+  Bound(pitch_lift,-M_PI_2,0);
+  float lift = sinf(pitch_lift)*9.81;
+  float T = cosf(pitch_lift)*-9.81;
+
+  // get the derivative of the lift wrt to theta
+  float liftd = guidance_indi_get_liftd(stateGetAirspeed_f(), eulers_zxy.theta);
+
+  Gmat[0][0] =  cphi*ctheta*spsi*T + cphi*spsi*lift;
+  Gmat[1][0] = -cphi*ctheta*cpsi*T - cphi*cpsi*lift;
+  Gmat[2][0] = -sphi*ctheta*T -sphi*lift;
+  Gmat[0][1] = (ctheta*cpsi - sphi*stheta*spsi)*T*GUIDANCE_INDI_PITCH_EFF_SCALING + sphi*spsi*liftd;
+  Gmat[1][1] = (ctheta*spsi + sphi*stheta*cpsi)*T*GUIDANCE_INDI_PITCH_EFF_SCALING - sphi*cpsi*liftd;
+  Gmat[2][1] = -cphi*stheta*T*GUIDANCE_INDI_PITCH_EFF_SCALING + cphi*liftd;
+  Gmat[0][2] = stheta*cpsi + sphi*ctheta*spsi;
+  Gmat[1][2] = stheta*spsi - sphi*ctheta*cpsi;
+  Gmat[2][2] = cphi*ctheta;
+
+  v_gih[0] = a_diff.x;
+  v_gih[1] = a_diff.y;
+  v_gih[2] = a_diff.z;
+}
 #endif
 /**
  *
