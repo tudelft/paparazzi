7ce7c8e9945386984878b5dd3e2775fc9edbb205
ziqingma <ziqing.ma9612@gmail.com>
2023-11-01 11:37:20 +0100

    fix zero effectiveness error

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
@@ -772,46 +772,7 @@ void guidance_indi_propagate_filters(void) {
  * @param a_diff acceleration errors in earth frame
  * @param body_v 3D vector to write the control objective v
  */
-<<<<<<< HEAD
-#if GUIDANCE_INDI_HYBRID_USE_WLS
-void WEAK guidance_indi_calcg_wing(float Gmat[GUIDANCE_INDI_HYBRID_V][GUIDANCE_INDI_HYBRID_U], struct FloatVect3 a_diff, float v_gih[GUIDANCE_INDI_HYBRID_V]) {
-
-  /*Pre-calculate sines and cosines*/
-  float sphi = sinf(eulers_zxy.phi);
-  float cphi = cosf(eulers_zxy.phi);
-  float stheta = sinf(eulers_zxy.theta);
-  float ctheta = cosf(eulers_zxy.theta);
-  float spsi = sinf(eulers_zxy.psi);
-  float cpsi = cosf(eulers_zxy.psi);
-  //minus gravity is a guesstimate of the thrust force, thrust measurement would be better
-
-  /*Amount of lift produced by the wing*/
-  float pitch_lift = eulers_zxy.theta;
-  Bound(pitch_lift,-M_PI_2,0);
-  float lift = sinf(pitch_lift)*9.81;
-  float T = cosf(pitch_lift)*-9.81;
-
-  // get the derivative of the lift wrt to theta
-  float liftd = guidance_indi_get_liftd(stateGetAirspeed_f(), eulers_zxy.theta);
-
-  Gmat[0][0] =  cphi*ctheta*spsi*T + cphi*spsi*lift;
-  Gmat[1][0] = -cphi*ctheta*cpsi*T - cphi*cpsi*lift;
-  Gmat[2][0] = -sphi*ctheta*T -sphi*lift;
-  Gmat[0][1] = (ctheta*cpsi - sphi*stheta*spsi)*T*guidance_indi_pitch_eff_scaling + sphi*spsi*liftd;
-  Gmat[1][1] = (ctheta*spsi + sphi*stheta*cpsi)*T*guidance_indi_pitch_eff_scaling - sphi*cpsi*liftd;
-  Gmat[2][1] = -cphi*stheta*T*guidance_indi_pitch_eff_scaling + cphi*liftd;
-  Gmat[0][2] = stheta*cpsi + sphi*ctheta*spsi;
-  Gmat[1][2] = stheta*spsi - sphi*ctheta*cpsi;
-  Gmat[2][2] = cphi*ctheta;
-
-  v_gih[0] = a_diff.x;
-  v_gih[1] = a_diff.y;
-  v_gih[2] = a_diff.z;
-}
-#elif defined(GUIDANCE_INDI_QUADPLANE)
-=======
 #if defined(GUIDANCE_INDI_QUADPLANE)
->>>>>>> fix bug that gave zero effectiveness matrix (#3151)
 /**
  * Perform WLS
  *
