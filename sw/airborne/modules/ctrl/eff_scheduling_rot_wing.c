826fe0d4a21b9e09ab1938bface2919e12ac37e6
Christophe De Wagter <dewagter@gmail.com>
2023-12-15 08:25:23 +0100

    Hide actuator driver number from code (#3206)
    * Hide actuator internal driver number. Should never be used in code.
    
    * Bugfix ... these should not be actutator_driver numbers but INDI output numbers

--- sw/airborne/modules/ctrl/eff_scheduling_rot_wing.c
+++ sw/airborne/modules/ctrl/eff_scheduling_rot_wing.c
@@ -257,6 +257,7 @@ void eff_scheduling_rot_wing_update_MMOI(void)
 
 void eff_scheduling_rot_wing_update_cmd(void)
 {
+  // These indexes depend on the INDI sequence, not the actuator IDX
   eff_sched_var.cmd_elevator = actuators_pprz[5];
   eff_sched_var.cmd_pusher_scaled = actuators_pprz[8] * 0.000853229; // Scaled with 8181 / 9600 / 1000
   eff_sched_var.cmd_T_mean_scaled = (actuators_pprz[0] + actuators_pprz[1] + actuators_pprz[2] + actuators_pprz[3]) / 4. * 0.000853229; // Scaled with 8181 / 9600 / 1000
