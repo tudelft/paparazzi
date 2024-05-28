605ac41cf4f345fe10c63c488c1f175001a50a36
TudorAvarvarei <doru.avarvarei@yahoo.com>
2024-05-28 12:00:02 +0200

    Working SNN

--- sw/airborne/modules/gcnet/gcnet_main.c
+++ sw/airborne/modules/gcnet/gcnet_main.c
@@ -124,7 +124,44 @@ int count = 0;
 float ekf_X[EKF_NUM_STATES];
 #endif
 
-// Build network
+// post, pre, w
+ConnectionConf const conf_inhid = {NUM_NODES, NUM_STATES, weights_in};
+
+// post, pre, w
+// const float *ptr_weights_out = &weights_out[0][0];
+ConnectionConf const conf_hidout = {NUM_CONTROLS, NUM_NODES, weights_out};
+
+// post, pre, w
+// const float *ptr_weights_hid_layers1_2 = &weights_hid_layers1_2[0][0];
+ConnectionConf const conf_hid_1 = {NUM_NODES, NUM_NODES, weights_hid_layers1_2};
+
+// post, pre, w
+// const float *ptr_weights_hid_layers2_3 = &weights_hid_layers2_3[0][0];
+ConnectionConf const conf_hid_2 = {NUM_NODES, NUM_NODES, weights_hid_layers2_3};
+
+// size, leak_i, leak_v, thresh, v_rest
+NeuronConf const conf_layer1 = {NUM_NODES, leak_i_layer1, leak_v_layer1, 
+                                thresh_layer1, 0.0f};
+
+// size, leak_i, leak_v, thresh, v_rest
+NeuronConf const conf_layer2 = {NUM_NODES, leak_i_layer2, leak_v_layer2, 
+                                thresh_layer2, 0.0f};
+
+// size, leak_i, leak_v, thresh, v_rest
+NeuronConf const conf_layer3 = {NUM_NODES, leak_i_layer3, leak_v_layer3, 
+                                thresh_layer3, 0.0f};
+
+const float out_scale_min = 3.000000000e+03;
+
+const float out_scale_max = 1.200000000e+04;
+
+// type, decoding_scale, centers, in_size, in_enc_size, hid_size, out_size,
+// inhid, hid, hidout, out
+NetworkConf const qz_conf = {in_norm_min, in_norm_max, out_scale_min, out_scale_max, 
+                          NUM_STATES, NUM_HIDDEN_LAYERS, NUM_NODES, NUM_CONTROLS,
+                          &conf_inhid, &conf_hid_1, &conf_hid_2, &conf_hidout,
+                          &conf_layer1, &conf_layer2, &conf_layer3};
+
 Network net;
 
 void gcnet_init(void)
--- sw/airborne/modules/gcnet/gcnet_main.h
+++ sw/airborne/modules/gcnet/gcnet_main.h
@@ -45,42 +45,32 @@
 // Include child structs
 
 // post, pre, w
-ConnectionConf const conf_inhid = {NUM_NODES, NUM_STATES, weights_in};
+extern ConnectionConf const conf_inhid;
 
 // post, pre, w
 // const float *ptr_weights_out = &weights_out[0][0];
-ConnectionConf const conf_hidout = {NUM_CONTROLS, NUM_NODES, weights_out};
+extern ConnectionConf const conf_hidout;
 
 // post, pre, w
 // const float *ptr_weights_hid_layers1_2 = &weights_hid_layers1_2[0][0];
-ConnectionConf const conf_hid_1 = {NUM_NODES, NUM_NODES, weights_hid_layers1_2};
+extern ConnectionConf const conf_hid_1;
 
 // post, pre, w
 // const float *ptr_weights_hid_layers2_3 = &weights_hid_layers2_3[0][0];
-ConnectionConf const conf_hid_2 = {NUM_NODES, NUM_NODES, weights_hid_layers2_3};
+extern ConnectionConf const conf_hid_2;
 
 // size, leak_i, leak_v, thresh, v_rest
-NeuronConf const conf_layer1 = {NUM_NODES, leak_i_layer1, leak_v_layer1, 
-                                thresh_layer1, 0.0f};
+extern NeuronConf const conf_layer1;
 
 // size, leak_i, leak_v, thresh, v_rest
-NeuronConf const conf_layer2 = {NUM_NODES, leak_i_layer2, leak_v_layer2, 
-                                thresh_layer2, 0.0f};
+extern NeuronConf const conf_layer2;
 
 // size, leak_i, leak_v, thresh, v_rest
-NeuronConf const conf_layer3 = {NUM_NODES, leak_i_layer3, leak_v_layer3, 
-                                thresh_layer3, 0.0f};
-
-const float out_scale_min = 3.000000000e+03;
-
-const float out_scale_max = 1.200000000e+04;
+extern NeuronConf const conf_layer3;
 
 // type, decoding_scale, centers, in_size, in_enc_size, hid_size, out_size,
 // inhid, hid, hidout, out
-NetworkConf const qz_conf = {in_norm_min, in_norm_max, out_scale_min, out_scale_max, 
-                          NUM_STATES, NUM_HIDDEN_LAYERS, NUM_NODES, NUM_CONTROLS,
-                          &conf_inhid, &conf_hid_1, &conf_hid_2, &conf_hidout,
-                          &conf_layer1, &conf_layer2, &conf_layer3};
+extern NetworkConf const qz_conf;
 
 extern float control_nn[NUM_CONTROLS];
 
