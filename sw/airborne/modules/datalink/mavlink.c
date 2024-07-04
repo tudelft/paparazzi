2619ae4b68d9206112111f896bcff8be944ab861
Freek van Tienen <freek.v.tienen@gmail.com>
2024-07-04 13:40:05 +0200

    Fix height

--- sw/airborne/modules/datalink/mavlink.c
+++ sw/airborne/modules/datalink/mavlink.c
@@ -607,7 +607,7 @@ void mavlink_common_message_handler(const mavlink_message_t *msg)
           struct LlaCoor_i lla;
           lla.lat = target.lat_int;
           lla.lon = target.lon_int;
-          lla.alt = MM_OF_M(target.alt);
+          lla.alt = -MM_OF_M(target.alt);
           ned_of_lla_point_i(&ned, &state.ned_origin_i, &lla);
           NED_FLOAT_OF_BFP(ned_f, ned);
           autopilot_guided_goto_ned(ned_f.x, ned_f.y, ned_f.z, target.yaw);
