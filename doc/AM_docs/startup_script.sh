ee322f7da2339e4b5e7bfbadc36e2b6e9934f0ca
alessandro-mancinelli <a.mancinelli@tudelft.nl>
2024-10-02 23:44:46 +0800

    updated vision code

--- doc/AM_docs/startup_script.sh
+++ doc/AM_docs/startup_script.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-sleep 2
-cd /home/orangepi/paparazzi/sw/ext/sds_software_suite_2024_04_09/examples/
-sudo ./custom_mode_am &
+
+#sleep 2
+#cd /home/orangepi/paparazzi/sw/ext/sds_software_suite_2024_04_09/examples/
+#sudo ./custom_mode_am &
+
 sleep 2
 cd /home/orangepi/paparazzi/sw/ext/nonlinear_CA/
 sudo ./Orangepi_PPZ_run &
 
+#sleep 2
+#cd /home/orangepi/paparazzi/sw/ext/aruco_detection/
+#python3 task_4_iter4.py
+
 #cd /home/orangepi/paparazzi/sw/ext/aruco_detection/
 #python3 ArucoMarker_Complete_V4_RealTime.py
+
