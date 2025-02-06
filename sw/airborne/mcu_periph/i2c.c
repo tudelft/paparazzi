tudelft/paparazzi

commit 713271e86b0a78c74b2c7dd3fb8dff4a1b27629d

[UART & I2C] factorize error telemetry functions. (#3426)
* [UART & I2C] factorize error telemetry functions.

* Fix test

---------

Co-authored-by: Fabien-B <Fabien-B@github.com>

sw/airborne/mcu_periph/i2c.c
@@ -28,44 +28,43 @@
 #include "mcu_periph/i2c.h"
 #include "mcu_periph/sys_time.h"
 
-#if PERIODIC_TELEMETRY
+#if PERIODIC_TELEMETRY && (USE_I2C0 || USE_I2C1 || USE_I2C2 || USE_I2C3 || USE_I2C4)
 #include "modules/datalink/telemetry.h"
+
+static void send_i2cx_err(struct transport_tx *trans, struct link_device *dev, struct i2c_periph* i2c)
+{
+  uint16_t i2c_wd_reset_cnt          = i2c->errors->wd_reset_cnt;
+  uint16_t i2c_queue_full_cnt        = i2c->errors->queue_full_cnt;
+  uint16_t i2c_ack_fail_cnt          = i2c->errors->ack_fail_cnt;
+  uint16_t i2c_miss_start_stop_cnt   = i2c->errors->miss_start_stop_cnt;
+  uint16_t i2c_arb_lost_cnt          = i2c->errors->arb_lost_cnt;
+  uint16_t i2c_over_under_cnt        = i2c->errors->over_under_cnt;
+  uint16_t i2c_pec_recep_cnt         = i2c->errors->pec_recep_cnt;
+  uint16_t i2c_timeout_tlow_cnt      = i2c->errors->timeout_tlow_cnt;
+  uint16_t i2c_smbus_alert_cnt       = i2c->errors->smbus_alert_cnt;
+  uint16_t i2c_unexpected_event_cnt  = i2c->errors->unexpected_event_cnt;
+  uint32_t i2c_last_unexpected_event = i2c->errors->last_unexpected_event;
+  uint8_t _bus = 0;
+  pprz_msg_send_I2C_ERRORS(trans, dev, AC_ID,
+                           &i2c_wd_reset_cnt,
+                           &i2c_queue_full_cnt,
+                           &i2c_ack_fail_cnt,
+                           &i2c_miss_start_stop_cnt,
+                           &i2c_arb_lost_cnt,
+                           &i2c_over_under_cnt,
+                           &i2c_pec_recep_cnt,
+                           &i2c_timeout_tlow_cnt,
+                           &i2c_smbus_alert_cnt,
+                           &i2c_unexpected_event_cnt,
+                           &i2c_last_unexpected_event,
+                           &_bus);
+}
 #endif
+
 
 #if USE_I2C0
 
 struct i2c_periph i2c0;
-
-#if PERIODIC_TELEMETRY
-static void send_i2c0_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t i2c0_wd_reset_cnt          = i2c0.errors->wd_reset_cnt;
-  uint16_t i2c0_queue_full_cnt        = i2c0.errors->queue_full_cnt;
-  uint16_t i2c0_ack_fail_cnt          = i2c0.errors->ack_fail_cnt;
-  uint16_t i2c0_miss_start_stop_cnt   = i2c0.errors->miss_start_stop_cnt;
-  uint16_t i2c0_arb_lost_cnt          = i2c0.errors->arb_lost_cnt;
-  uint16_t i2c0_over_under_cnt        = i2c0.errors->over_under_cnt;
-  uint16_t i2c0_pec_recep_cnt         = i2c0.errors->pec_recep_cnt;
-  uint16_t i2c0_timeout_tlow_cnt      = i2c0.errors->timeout_tlow_cnt;
-  uint16_t i2c0_smbus_alert_cnt       = i2c0.errors->smbus_alert_cnt;
-  uint16_t i2c0_unexpected_event_cnt  = i2c0.errors->unexpected_event_cnt;
-  uint32_t i2c0_last_unexpected_event = i2c0.errors->last_unexpected_event;
-  uint8_t _bus0 = 0;
-  pprz_msg_send_I2C_ERRORS(trans, dev, AC_ID,
-                           &i2c0_wd_reset_cnt,
-                           &i2c0_queue_full_cnt,
-                           &i2c0_ack_fail_cnt,
-                           &i2c0_miss_start_stop_cnt,
-                           &i2c0_arb_lost_cnt,
-                           &i2c0_over_under_cnt,
-                           &i2c0_pec_recep_cnt,
-                           &i2c0_timeout_tlow_cnt,
-                           &i2c0_smbus_alert_cnt,
-                           &i2c0_unexpected_event_cnt,
-                           &i2c0_last_unexpected_event,
-                           &_bus0);
-}
-#endif
 
 void i2c0_init(void)
 {
@@ -80,37 +79,6 @@ void i2c0_init(void)
 
 struct i2c_periph i2c1;
 
-#if PERIODIC_TELEMETRY
-static void send_i2c1_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t i2c1_wd_reset_cnt          = i2c1.errors->wd_reset_cnt;
-  uint16_t i2c1_queue_full_cnt        = i2c1.errors->queue_full_cnt;
-  uint16_t i2c1_ack_fail_cnt          = i2c1.errors->ack_fail_cnt;
-  uint16_t i2c1_miss_start_stop_cnt   = i2c1.errors->miss_start_stop_cnt;
-  uint16_t i2c1_arb_lost_cnt          = i2c1.errors->arb_lost_cnt;
-  uint16_t i2c1_over_under_cnt        = i2c1.errors->over_under_cnt;
-  uint16_t i2c1_pec_recep_cnt         = i2c1.errors->pec_recep_cnt;
-  uint16_t i2c1_timeout_tlow_cnt      = i2c1.errors->timeout_tlow_cnt;
-  uint16_t i2c1_smbus_alert_cnt       = i2c1.errors->smbus_alert_cnt;
-  uint16_t i2c1_unexpected_event_cnt  = i2c1.errors->unexpected_event_cnt;
-  uint32_t i2c1_last_unexpected_event = i2c1.errors->last_unexpected_event;
-  uint8_t _bus1 = 1;
-  pprz_msg_send_I2C_ERRORS(trans, dev, AC_ID,
-                           &i2c1_wd_reset_cnt,
-                           &i2c1_queue_full_cnt,
-                           &i2c1_ack_fail_cnt,
-                           &i2c1_miss_start_stop_cnt,
-                           &i2c1_arb_lost_cnt,
-                           &i2c1_over_under_cnt,
-                           &i2c1_pec_recep_cnt,
-                           &i2c1_timeout_tlow_cnt,
-                           &i2c1_smbus_alert_cnt,
-                           &i2c1_unexpected_event_cnt,
-                           &i2c1_last_unexpected_event,
-                           &_bus1);
-}
-#endif
-
 void i2c1_init(void)
 {
   i2c_init(&i2c1);
@@ -123,37 +91,6 @@ void i2c1_init(void)
 #if USE_I2C2
 
 struct i2c_periph i2c2;
-
-#if PERIODIC_TELEMETRY
-static void send_i2c2_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t i2c2_wd_reset_cnt          = i2c2.errors->wd_reset_cnt;
-  uint16_t i2c2_queue_full_cnt        = i2c2.errors->queue_full_cnt;
-  uint16_t i2c2_ack_fail_cnt          = i2c2.errors->ack_fail_cnt;
-  uint16_t i2c2_miss_start_stop_cnt   = i2c2.errors->miss_start_stop_cnt;
-  uint16_t i2c2_arb_lost_cnt          = i2c2.errors->arb_lost_cnt;
-  uint16_t i2c2_over_under_cnt        = i2c2.errors->over_under_cnt;
-  uint16_t i2c2_pec_recep_cnt         = i2c2.errors->pec_recep_cnt;
-  uint16_t i2c2_timeout_tlow_cnt      = i2c2.errors->timeout_tlow_cnt;
-  uint16_t i2c2_smbus_alert_cnt       = i2c2.errors->smbus_alert_cnt;
-  uint16_t i2c2_unexpected_event_cnt  = i2c2.errors->unexpected_event_cnt;
-  uint32_t i2c2_last_unexpected_event = i2c2.errors->last_unexpected_event;
-  uint8_t _bus2 = 2;
-  pprz_msg_send_I2C_ERRORS(trans, dev, AC_ID,
-                           &i2c2_wd_reset_cnt,
-                           &i2c2_queue_full_cnt,
-                           &i2c2_ack_fail_cnt,
-                           &i2c2_miss_start_stop_cnt,
-                           &i2c2_arb_lost_cnt,
-                           &i2c2_over_under_cnt,
-                           &i2c2_pec_recep_cnt,
-                           &i2c2_timeout_tlow_cnt,
-                           &i2c2_smbus_alert_cnt,
-                           &i2c2_unexpected_event_cnt,
-                           &i2c2_last_unexpected_event,
-                           &_bus2);
-}
-#endif
 
 void i2c2_init(void)
 {
@@ -173,37 +110,6 @@ void i2c3_init(void)
   i2c3_hw_init();
 }
 
-#if PERIODIC_TELEMETRY
-static void send_i2c3_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t i2c3_wd_reset_cnt          = i2c3.errors->wd_reset_cnt;
-  uint16_t i2c3_queue_full_cnt        = i2c3.errors->queue_full_cnt;
-  uint16_t i2c3_ack_fail_cnt          = i2c3.errors->ack_fail_cnt;
-  uint16_t i2c3_miss_start_stop_cnt   = i2c3.errors->miss_start_stop_cnt;
-  uint16_t i2c3_arb_lost_cnt          = i2c3.errors->arb_lost_cnt;
-  uint16_t i2c3_over_under_cnt        = i2c3.errors->over_under_cnt;
-  uint16_t i2c3_pec_recep_cnt         = i2c3.errors->pec_recep_cnt;
-  uint16_t i2c3_timeout_tlow_cnt      = i2c3.errors->timeout_tlow_cnt;
-  uint16_t i2c3_smbus_alert_cnt       = i2c3.errors->smbus_alert_cnt;
-  uint16_t i2c3_unexpected_event_cnt  = i2c3.errors->unexpected_event_cnt;
-  uint32_t i2c3_last_unexpected_event = i2c3.errors->last_unexpected_event;
-  uint8_t _bus3 = 3;
-  pprz_msg_send_I2C_ERRORS(trans, dev, AC_ID,
-                           &i2c3_wd_reset_cnt,
-                           &i2c3_queue_full_cnt,
-                           &i2c3_ack_fail_cnt,
-                           &i2c3_miss_start_stop_cnt,
-                           &i2c3_arb_lost_cnt,
-                           &i2c3_over_under_cnt,
-                           &i2c3_pec_recep_cnt,
-                           &i2c3_timeout_tlow_cnt,
-                           &i2c3_smbus_alert_cnt,
-                           &i2c3_unexpected_event_cnt,
-                           &i2c3_last_unexpected_event,
-                           &_bus3);
-}
-#endif
-
 #endif /* USE_I2C3 */
 
 #if USE_I2C4
@@ -215,37 +121,6 @@ void i2c4_init(void)
   i2c_init(&i2c4);
   i2c4_hw_init();
 }
-
-#if PERIODIC_TELEMETRY
-static void send_i2c4_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t i2c4_wd_reset_cnt          = i2c4.errors->wd_reset_cnt;
-  uint16_t i2c4_queue_full_cnt        = i2c4.errors->queue_full_cnt;
-  uint16_t i2c4_ack_fail_cnt          = i2c4.errors->ack_fail_cnt;
-  uint16_t i2c4_miss_start_stop_cnt   = i2c4.errors->miss_start_stop_cnt;
-  uint16_t i2c4_arb_lost_cnt          = i2c4.errors->arb_lost_cnt;
-  uint16_t i2c4_over_under_cnt        = i2c4.errors->over_under_cnt;
-  uint16_t i2c4_pec_recep_cnt         = i2c4.errors->pec_recep_cnt;
-  uint16_t i2c4_timeout_tlow_cnt      = i2c4.errors->timeout_tlow_cnt;
-  uint16_t i2c4_smbus_alert_cnt       = i2c4.errors->smbus_alert_cnt;
-  uint16_t i2c4_unexpected_event_cnt  = i2c4.errors->unexpected_event_cnt;
-  uint32_t i2c4_last_unexpected_event = i2c4.errors->last_unexpected_event;
-  uint8_t _bus4 = 4;
-  pprz_msg_send_I2C_ERRORS(trans, dev, AC_ID,
-                           &i2c4_wd_reset_cnt,
-                           &i2c4_queue_full_cnt,
-                           &i2c4_ack_fail_cnt,
-                           &i2c4_miss_start_stop_cnt,
-                           &i2c4_arb_lost_cnt,
-                           &i2c4_over_under_cnt,
-                           &i2c4_pec_recep_cnt,
-                           &i2c4_timeout_tlow_cnt,
-                           &i2c4_smbus_alert_cnt,
-                           &i2c4_unexpected_event_cnt,
-                           &i2c4_last_unexpected_event,
-                           &_bus4);
-}
-#endif
 
 #endif /* USE_I2C4 */
 
@@ -265,27 +140,27 @@ static void send_i2c_err(struct transport_tx *trans __attribute__((unused)),
   switch (_i2c_nb_cnt) {
     case 0:
 #if USE_I2C0
-      send_i2c0_err(trans, dev);
+      send_i2cx_err(trans, dev, &i2c0);
 #endif
       break;
     case 1:
 #if USE_I2C1
-      send_i2c1_err(trans, dev);
+      send_i2cx_err(trans, dev, &i2c1);
 #endif
       break;
     case 2:
 #if USE_I2C2
-      send_i2c2_err(trans, dev);
+      send_i2cx_err(trans, dev, &i2c2);
 #endif
       break;
     case 3:
 #if USE_I2C3
-      send_i2c3_err(trans, dev);
+      send_i2cx_err(trans, dev, &i2c3);
 #endif
       break;
     case 4:
 #if USE_I2C4
-      send_i2c4_err(trans, dev);
+      send_i2cx_err(trans, dev, &i2c4);
 #endif
     case 5:
 #if USE_SOFTI2C0

conf/modules/i2c.xml
@@ -15,6 +15,7 @@
     <file name="softi2c.c" dir="mcu_periph"/>
     <file_arch name="i2c_arch.c" dir="mcu_periph"/>
     <test>
+        <define name="USE_I2C2" value="TRUE"/>
     </test>
   </makefile>
 </module>

sw/airborne/mcu_periph/uart.c
@@ -22,159 +22,54 @@
 
 #include "mcu_periph/uart.h"
 
-#if PERIODIC_TELEMETRY
+#if PERIODIC_TELEMETRY && (USE_UART0 || USE_UART1 || USE_UART2 || USE_UART3 || USE_UART4 || USE_UART5 || USE_UART6 || USE_UART7 || USE_UART8)
 #include "modules/datalink/telemetry.h"
-#endif
 
-#if USE_UART0
-struct uart_periph uart0;
-
-#if PERIODIC_TELEMETRY
-static void send_uart0_err(struct transport_tx *trans, struct link_device *dev)
+static void send_uartx_err(struct transport_tx *trans, struct link_device *dev, struct uart_periph* uart)
 {
-  uint16_t ore    = uart0.ore;
-  uint16_t ne_err = uart0.ne_err;
-  uint16_t fe_err = uart0.fe_err;
+  uint16_t ore    = uart->ore;
+  uint16_t ne_err = uart->ne_err;
+  uint16_t fe_err = uart->fe_err;
   uint8_t _bus0 = 0;
   pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
                             &ore, &ne_err, &fe_err, &_bus0);
 }
 #endif
 
+#if USE_UART0
+struct uart_periph uart0;
 #endif
 
 #if USE_UART1
 struct uart_periph uart1;
-
-#if PERIODIC_TELEMETRY
-static void send_uart1_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart1.ore;
-  uint16_t ne_err = uart1.ne_err;
-  uint16_t fe_err = uart1.fe_err;
-  uint8_t _bus1 = 1;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus1);
-}
-#endif
-
 #endif
 
 #if USE_UART2
 struct uart_periph uart2;
-
-#if PERIODIC_TELEMETRY
-static void send_uart2_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart2.ore;
-  uint16_t ne_err = uart2.ne_err;
-  uint16_t fe_err = uart2.fe_err;
-  uint8_t _bus2 = 2;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus2);
-}
-#endif
-
 #endif
 
 #if USE_UART3
 struct uart_periph uart3;
-
-#if PERIODIC_TELEMETRY
-static void send_uart3_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart3.ore;
-  uint16_t ne_err = uart3.ne_err;
-  uint16_t fe_err = uart3.fe_err;
-  uint8_t _bus3 = 3;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus3);
-}
-#endif
-
 #endif
 
 #if USE_UART4
 struct uart_periph uart4;
-
-#if PERIODIC_TELEMETRY
-static void send_uart4_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart4.ore;
-  uint16_t ne_err = uart4.ne_err;
-  uint16_t fe_err = uart4.fe_err;
-  uint8_t _bus4 = 4;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus4);
-}
-#endif
-
 #endif
 
 #if USE_UART5
 struct uart_periph uart5;
-
-#if PERIODIC_TELEMETRY
-static void send_uart5_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart5.ore;
-  uint16_t ne_err = uart5.ne_err;
-  uint16_t fe_err = uart5.fe_err;
-  uint8_t _bus5 = 5;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus5);
-}
-#endif
-
 #endif
 
 #if USE_UART6
 struct uart_periph uart6;
-
-#if PERIODIC_TELEMETRY
-static void send_uart6_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart6.ore;
-  uint16_t ne_err = uart6.ne_err;
-  uint16_t fe_err = uart6.fe_err;
-  uint8_t _bus6 = 6;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus6);
-}
-#endif
-
 #endif
 
 #if USE_UART7
 struct uart_periph uart7;
-
-#if PERIODIC_TELEMETRY
-static void send_uart7_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart7.ore;
-  uint16_t ne_err = uart7.ne_err;
-  uint16_t fe_err = uart7.fe_err;
-  uint8_t _bus7 = 7;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus7);
-}
-#endif
 #endif
 
 #if USE_UART8
 struct uart_periph uart8;
-
-#if PERIODIC_TELEMETRY
-static void send_uart8_err(struct transport_tx *trans, struct link_device *dev)
-{
-  uint16_t ore    = uart8.ore;
-  uint16_t ne_err = uart8.ne_err;
-  uint16_t fe_err = uart8.fe_err;
-  uint8_t _bus8 = 8;
-  pprz_msg_send_UART_ERRORS(trans, dev, AC_ID,
-                            &ore, &ne_err, &fe_err, &_bus8);
-}
-#endif
 #endif
 
 #if PERIODIC_TELEMETRY
@@ -185,39 +80,39 @@ static void send_uart_err(struct transport_tx *trans __attribute__ ((unused)),
   switch (uart_nb_cnt) {
 #if USE_UART0
     case 0:
-      send_uart0_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart0); break;
 #endif
 #if USE_UART1
     case 1:
-      send_uart1_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart1); break;
 #endif
 #if USE_UART2
     case 2:
-      send_uart2_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart2); break;
 #endif
 #if USE_UART3
     case 3:
-      send_uart3_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart3); break;
 #endif
 #if USE_UART4
     case 4:
-      send_uart4_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart4); break;
 #endif
 #if USE_UART5
     case 5:
-      send_uart5_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart5); break;
 #endif
 #if USE_UART6
     case 6:
-      send_uart6_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart6); break;
 #endif
 #if USE_UART7
     case 7:
-      send_uart7_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart7); break;
 #endif
 #if USE_UART8
     case 8:
-      send_uart8_err(trans, dev); break;
+      send_uartx_err(trans, dev, &uart8); break;
 #endif
     default: break;
   }

commit 672e0c49c6ed5af5255a721824f73c5dde7fbc5c

[I2C] Fix bug introduced in #3426. (#3427)
Manifesting if no I2C is used.

Co-authored-by: Fabien-B <Fabien-B@github.com>

sw/airborne/mcu_periph/i2c.c
@@ -28,7 +28,10 @@
 #include "mcu_periph/i2c.h"
 #include "mcu_periph/sys_time.h"
 
-#if PERIODIC_TELEMETRY && (USE_I2C0 || USE_I2C1 || USE_I2C2 || USE_I2C3 || USE_I2C4)
+#define USE_I2C (USE_I2C0 || USE_I2C1 || USE_I2C2 || USE_I2C3 || USE_I2C4)
+#define USE_SOFT_I2C (USE_SOFTI2C0 || USE_SOFTI2C1)
+
+#if PERIODIC_TELEMETRY && (USE_I2C || USE_SOFT_I2C)
 #include "modules/datalink/telemetry.h"
 
 static void send_i2cx_err(struct transport_tx *trans, struct link_device *dev, struct i2c_periph* i2c)
@@ -132,7 +135,7 @@ extern void send_softi2c0_err(struct transport_tx *trans, struct link_device *de
 extern void send_softi2c1_err(struct transport_tx *trans, struct link_device *dev);
 #endif /* USE_SOFTI2C1 */
 
-#if PERIODIC_TELEMETRY
+#if PERIODIC_TELEMETRY && (USE_I2C || USE_SOFT_I2C)
 static void send_i2c_err(struct transport_tx *trans __attribute__((unused)),
                          struct link_device *dev __attribute__((unused)))
 {
@@ -189,7 +192,7 @@ void i2c_init(struct i2c_periph *p)
   p->status = I2CIdle;
   p->reg_addr = NULL;
 
-#if PERIODIC_TELEMETRY
+#if PERIODIC_TELEMETRY && (USE_I2C || USE_SOFT_I2C)
   // the first to register do it for the others
   register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_I2C_ERRORS, send_i2c_err);
 #endif