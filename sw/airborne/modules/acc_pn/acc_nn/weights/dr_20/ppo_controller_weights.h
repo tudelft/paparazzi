ac7cac909fb6e91f5d2f6811035f2dcde4524b07
MerlijnBroekers2 <merlijnbroekers@icloud.com>
2025-08-19 12:30:36 +0200

    ctbr and motor dr weights

---
+++ sw/airborne/modules/acc_pn/acc_nn/weights/dr_20/ppo_controller_weights.h
@@ -0,0 +1,30 @@
+#ifndef PPO_CONTROLLER_WEIGHTS_H
+#define PPO_CONTROLLER_WEIGHTS_H
+
+extern float log_std[];
+
+extern float mlp_extractor_policy_net_0_weight[];
+
+extern float mlp_extractor_policy_net_0_bias[];
+
+extern float mlp_extractor_policy_net_2_weight[];
+
+extern float mlp_extractor_policy_net_2_bias[];
+
+extern float mlp_extractor_value_net_0_weight[];
+
+extern float mlp_extractor_value_net_0_bias[];
+
+extern float mlp_extractor_value_net_2_weight[];
+
+extern float mlp_extractor_value_net_2_bias[];
+
+extern float action_net_weight[];
+
+extern float action_net_bias[];
+
+extern float value_net_weight[];
+
+extern float value_net_bias[];
+
+#endif // PPO_CONTROLLER_WEIGHTS_H
