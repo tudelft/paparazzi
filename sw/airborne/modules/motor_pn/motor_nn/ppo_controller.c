c05a222c39f2f471fbc2539afc8d195a7e4f9ce2
MerlijnBroekers2 <merlijnbroekers@icloud.com>
2025-10-07 10:38:51 +0200

    gamma 15 dr 10 weights

--- sw/airborne/modules/motor_pn/motor_nn/ppo_controller.c
+++ sw/airborne/modules/motor_pn/motor_nn/ppo_controller.c
@@ -1,7 +1,7 @@
 // ppo_controller.c
 
 #include <math.h>
-#include "weights/gamma_40_dr_10/ppo_controller_weights.h"
+#include "weights/gamma_15_dr_10/ppo_controller_weights.h"
 #include "ppo_controller.h"
 
 #define INPUT_DIM 21
