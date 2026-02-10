00e8a2287f40adc54b9f021b517d1122729c4a89
ntouev <49869786+ntouev@users.noreply.github.com>
2026-02-10 14:37:54 +0100

    acceleration logginf -> float & att -> ZXY

--- sw/airborne/modules/loggers/logger_file.c
+++ sw/airborne/modules/loggers/logger_file.c
@@ -92,16 +92,24 @@ static void logger_file_write_header(FILE *file) {
  * @param file Log file pointer
  */
 static void logger_file_write_row(FILE *file) {
+  struct Int32Vect3 *acc_i;
+  struct FloatVect3 *acc_f;
+
   struct NedCoor_f *pos = stateGetPositionNed_f();
   struct NedCoor_f *vel = stateGetSpeedNed_f();
-  struct Int32Vect3 *acc = stateGetAccelBody_i();
-  struct FloatEulers *att = stateGetNedToBodyEulers_f();
+
+  acc_i = stateGetAccelBody_i();
+  ACCELS_FLOAT_OF_BFP(*acc_f, *acc_i);
+  
+  struct FloatEulers *att;
+  float_eulers_of_quat_zxy(att, stateGetNedToBodyQuat_f());
+  
   struct FloatRates *rates = stateGetBodyRates_f();
 
   fprintf(file, "%f,", get_sys_time_float());
   fprintf(file, "%f,%f,%f,", pos->x, pos->y, pos->z);
   fprintf(file, "%f,%f,%f,", vel->x, vel->y, vel->z);
-  fprintf(file, "%d,%d,%d,", acc->x, acc->y, acc->z);
+  fprintf(file, "%d,%d,%d,", acc_f->x, acc_f->y, acc_f->z);
   fprintf(file, "%f,%f,%f,", att->phi, att->theta, att->psi);
   fprintf(file, "%f,%f,%f,", rates->p, rates->q, rates->r);
 #ifdef BOARD_BEBOP
