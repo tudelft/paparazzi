f023382dd8a46e3f0de3f6b6d64052acaf26a881
ntouev <49869786+ntouev@users.noreply.github.com>
2026-02-27 11:39:29 +0100

    can start testing now

--- sw/airborne/modules/flatness/flatness_stabilization.c
+++ sw/airborne/modules/flatness/flatness_stabilization.c
@@ -30,6 +30,8 @@
 
 // #include "modules/datalink/telemetry.h"
 
+#define SAFE_SQRT(x) (sqrt((x) > 0 ? (x) : 0.0))
+
 typedef struct {
     float x;
     float y;
@@ -42,14 +44,16 @@ static const float MU_X = 70  / PHI_COEFF_DIV;
 static const float MU_Y = 150 / PHI_COEFF_DIV;
 static const float MU_Z = 15  / PHI_COEFF_DIV;
 static const float C_Z  = -5  / PHI_COEFF_DIV;
-static const float ACT_CUTOFF_FREQ = 11.0f;
-static const float FILT_CUTOFF = 5;
+static const float ACT_CUTOFF_OMEGA = 11.0f;
+static const float FILT_CUTOFF_FREQ = 5;
 static const Gain_t Kq = {1.0f, 1.0f, 1.0f};
 static const Gain_t Komega = {5.0f, 5.0f, 5.0f};
 
-// global vars
+// global vars declared as extern in header file
 dbg_t dbg;
 struct Fl_stabilization fl_stabilization;
+
+// global vars
 static Butterworth2LowPass act_filter[ACTUATORS_NB];
 static Butterworth2LowPass rates_num_der_filter[3];
 static float ACT_DYN_ALPHA;
@@ -61,15 +65,17 @@ static struct FloatRates ang_accel_sp = {0., 0., 0.};
 static float ang_accel_filt[3] = {0., 0., 0.};
 
 // helper functions
-float discrete_first_order_filter(float, float, float);
-void expose_dbg_variables(struct FloatQuat *, struct FloatQuat *, 
+static float discrete_first_order_filter(float, float, float);
+static void forw_rot_flatness(float *, float *);
+static void inv_rot_flatness(float, float *, float *);
+static void expose_dbg_variables(struct FloatQuat *, struct FloatQuat *, 
                           struct FloatRates *, struct FloatRates *,
                           struct FloatRates *, float *,
                           Act_t *);
-void forw_rot_flatness(float *, float *);
-void inv_rot_flatness(float, float *, float *);
 
-void expose_dbg_variables(struct FloatQuat *quat, struct FloatQuat *quat_sp,
+// -------- CODE ---------- //
+
+static void expose_dbg_variables(struct FloatQuat *quat, struct FloatQuat *quat_sp,
                           struct FloatRates *rates, struct FloatRates *rates_sp,
                           struct FloatRates *ang_accel_sp, float *ang_accel_filt,
                           Act_t *act)
@@ -85,7 +91,7 @@ void expose_dbg_variables(struct FloatQuat *quat, struct FloatQuat *quat_sp,
 
 void flatness_stabilization_init(void)
 {
-    float tau = 1.0 / (2.0 * M_PI * FILT_CUTOFF);
+    float tau = 1.0 / (2.0 * M_PI * FILT_CUTOFF_FREQ);
     float sample_time = 1.0 / PERIODIC_FREQUENCY;
 
     for (int i = 0; i < ACTUATORS_NB; i++) {
@@ -96,99 +102,99 @@ void flatness_stabilization_init(void)
         init_butterworth_2_low_pass(&rates_num_der_filter[i], tau, sample_time, 0.0);
     }
 
-    ACT_DYN_ALPHA = exp(-ACT_CUTOFF_FREQ/PERIODIC_FREQUENCY);
+    ACT_DYN_ALPHA = exp(-ACT_CUTOFF_OMEGA/PERIODIC_FREQUENCY);
 }
 
-void flatness_stabilization_run(bool in_flight, struct StabilizationSetpoint *att_sp, struct ThrustSetpoint *thrust, int32_t *cmd)
+void flatness_stabilization_run(bool UNUSED in_flight, struct StabilizationSetpoint *att_sp, struct ThrustSetpoint *thrust, int32_t *cmd)
 {
 
+    // calculate quaternion error
     struct FloatQuat quat_err;
-    if (in_flight) {
+    quat_sp = stab_sp_to_quat_f(att_sp);
+    quat = stateGetNedToBodyQuat_f();
+    float_quat_inv_comp_norm_shortest(&quat_err, quat, &quat_sp);
 
-        // calculate quaternion error
-        quat_sp = stab_sp_to_quat_f(att_sp);
-        quat = stateGetNedToBodyQuat_f();
-        float_quat_inv_comp_norm_shortest(&quat_err, quat, &quat_sp);
+    // calculate rates setpoint
+    rates_sp.p = 2 * Kq.x * quat_err.qx;
+    rates_sp.q = 2 * Kq.y * quat_err.qy;
+    rates_sp.r = 2 * Kq.z * quat_err.qz;
 
-        // calculate rates setpoint
-        rates_sp.p = 2 * Kq.x * quat_err.qx;
-        rates_sp.q = 2 * Kq.y * quat_err.qy;
-        rates_sp.r = 2 * Kq.z * quat_err.qz;
+    // add FF rate sp
+    // struct FloatRates rate_ff = stab_sp_to_rates_f(att_sp);
+    // RATES_ADD(rates_sp, rate_ff);
 
-        // add FF rate sp
-        // struct FloatRates rate_ff = stab_sp_to_rates_f(att_sp);
-        // RATES_ADD(rates_sp, rate_ff);
+    // calculate angular acceleration setpoint
+    rates = stateGetBodyRates_f();
+    ang_accel_sp.p = Komega.x * (rates_sp.p - rates->p);
+    ang_accel_sp.q = Komega.y * (rates_sp.q - rates->q);
+    ang_accel_sp.r = Komega.z * (rates_sp.r - rates->r);
 
-        // calculate angular acceleration setpoint
-        rates = stateGetBodyRates_f();
-        ang_accel_sp.p = Komega.x * (rates_sp.p - rates->p);
-        ang_accel_sp.q = Komega.y * (rates_sp.q - rates->q);
-        ang_accel_sp.r = Komega.z * (rates_sp.r - rates->r);
-
-        // butterworth filter for actuators    
-        for (int i = 0; i < ACTUATORS_NB; i++) {
-            act.state[i] = discrete_first_order_filter(ACT_DYN_ALPHA, act.cmd[i], act.state[i]);
-            update_butterworth_2_low_pass(&act_filter[i], act.state[i]);
-            act.state_filt[i] = act_filter[i].o[0];
-        }
-        
-        // numerical derivative + butterworth filter for angular acceleration
-        float rates_vector[3] = {rates->p, rates->q, rates->r};
-        for (int i = 0; i < 3; i++) {
-            update_butterworth_2_low_pass(&rates_num_der_filter[i], rates_vector[i]);
-            ang_accel_filt[i] = (rates_num_der_filter[i].o[0] - rates_num_der_filter[i].o[1]) * PERIODIC_FREQUENCY;
-        }
-
-        // forward rotational flatness
-        float m_filt[3];
-        forw_rot_flatness(act.state_filt, m_filt);
-
-        // incremental law
-        float m_cmd[3];
-        float ang_accel_sp_vector[3] = {ang_accel_sp.p, ang_accel_sp.q, ang_accel_sp.r};
-        for (int i = 0; i < 3; i++) {
-            m_cmd[i] = (ang_accel_sp_vector[i] - ang_accel_filt[i]) + m_filt[i];
-        }
-
-        // inverse rotational flatness
-        // float specific_thrust = -10; // m/s^2
-        float specific_thrust = -((float)thrust->sp.thrust_i[THRUST_AXIS_Z])*10.0/5700.0;
-        inv_rot_flatness(specific_thrust, m_cmd, act.cmd);
-
-        // assign commands
-        for (int i = 0; i < ACTUATORS_NB; i++) {
-            actuators_pprz[i] = act.cmd[i];
-        }
+    // actuator state estimation + butterworth filter    
+    for (int i = 0; i < ACTUATORS_NB; i++) {
+        act.state[i] = discrete_first_order_filter(ACT_DYN_ALPHA, act.cmd[i], act.state[i]);
+        update_butterworth_2_low_pass(&act_filter[i], act.state[i]);
+        act.state_filt[i] = act_filter[i].o[0];
+    }
     
+    // angular acceleration numerical calculation + butterworth filter
+    float rates_vector[3] = {rates->p, rates->q, rates->r};
+    for (int i = 0; i < 3; i++) {
+        update_butterworth_2_low_pass(&rates_num_der_filter[i], rates_vector[i]);
+        ang_accel_filt[i] = (rates_num_der_filter[i].o[0] - rates_num_der_filter[i].o[1]) * PERIODIC_FREQUENCY;
     }
 
+    // forward rotational flatness
+    float m_filt[3];
+    forw_rot_flatness(act.state_filt, m_filt);
+
+    // incremental law
+    float m_cmd[3];
+    float ang_accel_sp_vector[3] = {ang_accel_sp.p, ang_accel_sp.q, ang_accel_sp.r};
+    for (int i = 0; i < 3; i++) {
+        m_cmd[i] = (ang_accel_sp_vector[i] - ang_accel_filt[i]) + m_filt[i];
+    }
+
+    // This comes from the guidance code or by the rc controller (map 0-9600 -> 0-2g)
+    float specific_thrust = -(float)(2*9.81/9600)*thrust->sp.thrust_i[THRUST_AXIS_Z];
+
+    // inverse rotational flatness
+    inv_rot_flatness(specific_thrust, m_cmd, act.cmd);
+
+    // assign commands
+    for (int i = 0; i < ACTUATORS_NB; i++) {
+        actuators_pprz[i] = act.cmd[i];
+    }
+    
     cmd[COMMAND_THRUST] = thrust->sp.thrust_i[THRUST_AXIS_Z];
     stabilization.cmd[COMMAND_THRUST] = cmd[COMMAND_THRUST]; // for autopilot_check_in_flight()
 
-    // printf("%.0f\t%.0f\t%.0f\t%.0f\t\t", act.cmd[0], act.cmd[1], act.cmd[2], act.cmd[3]);
+    // printf("%d\t",in_flight);
+    // printf("%.0f\t%.0f\t%.0f\t%.0f", act.cmd[0], act.cmd[1], act.cmd[2], act.cmd[3]);
+    // printf("\n");
 
     expose_dbg_variables(quat, &quat_sp, rates, &rates_sp, &ang_accel_sp, ang_accel_filt, &act);
 }
 
-float discrete_first_order_filter(float alpha, float input, float prev_output)
+static float discrete_first_order_filter(float alpha, float input, float prev_output)
 {  
+    // y_{k} = a*y_{k-1} + (1 - a)*x_{k}
     float output = alpha*prev_output + (1 - alpha) * input;
 
     return output;
 }
 
-void forw_rot_flatness(float *u, float *m)
+static void forw_rot_flatness(float *u, float *m)
 {
     m[0] = MU_X * (u[0]*u[0] - u[1]*u[1] - u[2]*u[2] + u[3]*u[3]);
     m[1] = MU_Y * (u[0]*u[0] + u[1]*u[1] - u[2]*u[2] - u[3]*u[3]);
     m[2] = MU_Z * (-u[0]*u[0] + u[1]*u[1] - u[2]*u[2] + u[3]*u[3]);
 }
 
-void inv_rot_flatness(float tau, float *m, float *u)
+static void inv_rot_flatness(float tau, float *m, float *u)
 {
     // I could do that with matrix inverse
-    u[0] = sqrt((tau/C_Z + m[0]/MU_X + m[1]/MU_Y - m[2]/MU_Z)/4);
-    u[1] = sqrt((tau/C_Z - m[0]/MU_X + m[1]/MU_Y + m[2]/MU_Z)/4);
-    u[2] = sqrt((tau/C_Z - m[0]/MU_X - m[1]/MU_Y - m[2]/MU_Z)/4);
-    u[3] = sqrt((tau/C_Z + m[0]/MU_X - m[1]/MU_Y + m[2]/MU_Z)/4);
+    u[0] = SAFE_SQRT((tau/C_Z + m[0]/MU_X + m[1]/MU_Y - m[2]/MU_Z)/ACTUATORS_NB);
+    u[1] = SAFE_SQRT((tau/C_Z - m[0]/MU_X + m[1]/MU_Y + m[2]/MU_Z)/ACTUATORS_NB);
+    u[2] = SAFE_SQRT((tau/C_Z - m[0]/MU_X - m[1]/MU_Y - m[2]/MU_Z)/ACTUATORS_NB);
+    u[3] = SAFE_SQRT((tau/C_Z + m[0]/MU_X - m[1]/MU_Y + m[2]/MU_Z)/ACTUATORS_NB);
 }
