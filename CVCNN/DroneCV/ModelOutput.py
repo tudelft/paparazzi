9bceb1fc8040765e989ffb3392421f22f1b94870
wsboer <W.S.Boer@student.tudelft.nl>
2026-03-10 20:40:53 +0100

    Retrained model
    Retrained model with 50 epochs, lr of 0.001, smaller batch size. Took 10% of data out as a test set. Don't know about losses but qualitatively looks like somewhat decent depthmaps. maybe a bit more polishing. test_filenames.json is just the filenames that were used for test set so should update this accordingly (same for depth_mmodel.pth)

---
+++ CVCNN/DroneCV/ModelOutput.py
@@ -0,0 +1,67 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+from ComputerVision import CNN
+import torch
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import json
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+model = CNN()
+model_path = "depth_model (1).pth"
+
+state_dict = torch.load(model_path, map_location=device)
+model.load_state_dict(state_dict)
+model = model.to(device)
+model.eval()
+
+transform = transforms.ToTensor()
+
+# test_path = 'Data/Images/01633.jpg' #Checking image from training data (More debugging than testing)
+# image = Image.open(test_path).convert("RGB")
+# image = transform(image)
+# image = image.unsqueeze(0).to(device)
+
+with open("test_filenames.json", "r") as f:
+    test_data = json.load(f)
+
+# with torch.no_grad():
+#     pred = model(image)
+
+
+
+for file in test_data[:10]:
+    path = 'Data/Images/{}'.format(file)
+    img = Image.open(path).convert("RGB")
+    img = transform(img)
+    img = img.unsqueeze(0).to(device)
+    original = 'Data/datasets_depth/depth_map/{}_depth.png'.format(file[:5])
+    original = Image.open(original)
+
+    with torch.no_grad():
+        pred = model(img)
+
+    depth = pred.squeeze().cpu().numpy()
+    depth = (depth - depth.min()) / (depth.max() - depth.min())
+    
+    plt.subplot(1,2,1)
+    plt.imshow(depth, cmap = 'gray')
+
+    plt.subplot(1,2,2)
+    plt.imshow(original)
+    plt.colorbar()
+
+    plt.show()
+
+
+
+# plt.imshow(depth, cmap="gray")
+# plt.colorbar()
+# plt.show()
+
+
+
