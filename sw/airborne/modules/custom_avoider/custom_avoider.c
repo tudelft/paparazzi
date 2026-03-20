030728c576ecca019890abf36c51686b4af68247
tommasocalzolari <tommi.calzolari@gmail.com>
2026-03-20 09:54:05 +0100

    final version for friday

--- sw/airborne/modules/custom_avoider/custom_avoider.c
+++ sw/airborne/modules/custom_avoider/custom_avoider.c
@@ -84,6 +84,14 @@ static int32_t gate_quality = 0;
 #define GATE_FUSION_ALIGN_HEADING_INC_DEG 4.0f
 #endif
 
+#ifndef GATE_FUSION_DEBUG
+#define GATE_FUSION_DEBUG 1
+#endif
+
+#ifndef GATE_FUSION_DEBUG_PERIOD
+#define GATE_FUSION_DEBUG_PERIOD 20
+#endif
+
 /* --------------------------------------------------------- */
 /* State machine                                             */
 /* --------------------------------------------------------- */
@@ -98,6 +106,7 @@ enum navigation_state_t {
 static enum navigation_state_t navigation_state = SEARCH_FOR_GATE;
 static float heading_increment = 5.f;
 static uint8_t blind_cycles_remaining = 0;
+static uint16_t debug_cycle_count = 0;
 
 /* --------------------------------------------------------- */
 /* Forward declarations                                      */
@@ -109,6 +118,15 @@ static uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor);
 static uint8_t increase_nav_heading(float incrementDegrees);
 static uint8_t gate_is_good(void);
 static uint8_t gate_is_close(void);
+static const char *navigation_state_name(enum navigation_state_t state);
+static void set_navigation_state(enum navigation_state_t new_state, const char *reason);
+static void debug_print_periodic_status(const char *phase, int16_t err_x);
+
+#if GATE_FUSION_DEBUG
+#define GATE_DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+#define GATE_DEBUG_PRINT(...) do { } while (0)
+#endif
 
 /* --------------------------------------------------------- */
 /* ABI callback                                              */
@@ -137,10 +155,17 @@ void navigation_controller_init(void)
 {
   navigation_state = SEARCH_FOR_GATE;
   blind_cycles_remaining = 0;
+  debug_cycle_count = 0;
 
   AbiBindMsgVISUAL_DETECTION(GATE_FUSION_VISUAL_DETECTION_ID,
                              &gate_ev,
                              gate_detection_cb);
+
+  GATE_DEBUG_PRINT("[gate_nav] init: min_q=%ld blind_q=%ld align_tol=%d center_x=%d\n",
+                   (long)GATE_FUSION_MIN_QUALITY,
+                   (long)GATE_FUSION_BLIND_QUALITY,
+                   GATE_FUSION_ALIGN_TOL_PX,
+                   GATE_FUSION_IMAGE_CENTER_X);
 }
 
 /* --------------------------------------------------------- */
@@ -153,43 +178,49 @@ void navigation_controller_periodic(void)
     return;
   }
 
+  debug_cycle_count++;
+
   switch (navigation_state) {
 
     case SEARCH_FOR_GATE:
       increase_nav_heading(heading_increment);
+      debug_print_periodic_status("search", gate_px - GATE_FUSION_IMAGE_CENTER_X);
 
       if (gate_is_good()) {
-        navigation_state = ALIGN_TO_GATE;
+        set_navigation_state(ALIGN_TO_GATE, "gate acquired");
       }
       break;
 
     case ALIGN_TO_GATE: {
       if (!gate_is_good()) {
-        navigation_state = SEARCH_FOR_GATE;
+        set_navigation_state(SEARCH_FOR_GATE, "lost gate during align");
         break;
       }
 
       int16_t err_x = gate_px - GATE_FUSION_IMAGE_CENTER_X;
+      debug_print_periodic_status("align", err_x);
 
       if (err_x < -GATE_FUSION_ALIGN_TOL_PX) {
         increase_nav_heading(GATE_FUSION_ALIGN_HEADING_INC_DEG);
       } else if (err_x > GATE_FUSION_ALIGN_TOL_PX) {
         increase_nav_heading(-GATE_FUSION_ALIGN_HEADING_INC_DEG);
       } else {
-        navigation_state = FLY_TO_GATE;
+        set_navigation_state(FLY_TO_GATE, "gate centered");
       }
       break;
     }
 
     case FLY_TO_GATE:
       if (!gate_is_good()) {
-        navigation_state = SEARCH_FOR_GATE;
+        set_navigation_state(SEARCH_FOR_GATE, "lost gate during approach");
         break;
       }
 
+      debug_print_periodic_status("fly", gate_px - GATE_FUSION_IMAGE_CENTER_X);
+
       if (gate_is_close()) {
         blind_cycles_remaining = GATE_FUSION_BLIND_CYCLES;
-        navigation_state = BLIND_THROUGH_GATE;
+        set_navigation_state(BLIND_THROUGH_GATE, "gate close enough");
         break;
       }
 
@@ -206,12 +237,12 @@ void navigation_controller_periodic(void)
       }
 
       if (blind_cycles_remaining == 0) {
-        navigation_state = SEARCH_FOR_GATE;
+        set_navigation_state(SEARCH_FOR_GATE, "blind pass complete");
       }
       break;
 
     default:
-      navigation_state = SEARCH_FOR_GATE;
+      set_navigation_state(SEARCH_FOR_GATE, "invalid state");
       break;
   }
 }
@@ -232,6 +263,65 @@ static uint8_t gate_is_close(void)
   return (gate_quality >= GATE_FUSION_BLIND_QUALITY ||
           gate_pw >= GATE_FUSION_BLIND_MIN_WIDTH ||
           gate_ph >= GATE_FUSION_BLIND_MIN_HEIGHT);
+}
+
+static const char *navigation_state_name(enum navigation_state_t state)
+{
+  switch (state) {
+    case SEARCH_FOR_GATE:
+      return "SEARCH";
+    case ALIGN_TO_GATE:
+      return "ALIGN";
+    case FLY_TO_GATE:
+      return "FLY";
+    case BLIND_THROUGH_GATE:
+      return "BLIND";
+    default:
+      return "UNKNOWN";
+  }
+}
+
+static void set_navigation_state(enum navigation_state_t new_state, const char *reason)
+{
+  if (navigation_state == new_state) {
+    return;
+  }
+
+  GATE_DEBUG_PRINT("[gate_nav] %s -> %s: %s (q=%ld px=%d w=%d h=%d blind=%u)\n",
+                   navigation_state_name(navigation_state),
+                   navigation_state_name(new_state),
+                   reason,
+                   (long)gate_quality,
+                   gate_px,
+                   gate_pw,
+                   gate_ph,
+                   blind_cycles_remaining);
+
+  navigation_state = new_state;
+}
+
+static void debug_print_periodic_status(const char *phase, int16_t err_x)
+{
+#if GATE_FUSION_DEBUG
+  if ((debug_cycle_count % GATE_FUSION_DEBUG_PERIOD) != 0) {
+    return;
+  }
+
+  GATE_DEBUG_PRINT("[gate_nav] %s: state=%s det=%u good=%u close=%u q=%ld px=%d err=%d w=%d h=%d\n",
+                   phase,
+                   navigation_state_name(navigation_state),
+                   gate_detected,
+                   gate_is_good(),
+                   gate_is_close(),
+                   (long)gate_quality,
+                   gate_px,
+                   err_x,
+                   gate_pw,
+                   gate_ph);
+#else
+  (void)phase;
+  (void)err_x;
+#endif
 }
 
 static uint8_t increase_nav_heading(float incrementDegrees)
