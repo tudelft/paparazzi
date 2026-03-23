d0aef377cf6ca10e1586de1c1addbba05e625636
ntouev <49869786+ntouev@users.noreply.github.com>
2026-03-23 14:51:41 +0100

    get rid of "No SRTM data found to check altitude." warning

--- sw/tools/generators/gen_flight_plan.ml
+++ sw/tools/generators/gen_flight_plan.ml
@@ -107,7 +107,7 @@ let check_altitude_srtm = fun a x wgs84 ->
       fprintf stderr "MAJOR NOTICE: below SRTM ground altitude (%.0f<%.0f) in %s\n" a srtm_alt (Xml.to_string x)
     end
   with Srtm.Tile_not_found e ->
-    fprintf stderr "No SRTM data found to check altitude.\n"
+    fprintf stderr ""
 
 let check_altitude = fun a x ->
   if a < !ground_alt +. !security_height then begin
