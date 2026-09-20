e50cbea2c4e46421f6574ec1193b9b3d6deac462
Christophe De Wagter <dewagter@gmail.com>
2026-09-21 00:03:55 +0200

    Imav 2026 outdoor integrate EH (#124)
    * WIP a lot of various issue fix
    
    * WIP MS4 simple flightplan
    
    ---------
    
    Co-authored-by: OpenUAS <OpenUAS@users.noreply.github.com>

---
+++ sw/airborne/modules/digital_cam/catia/doxygen/html/structcaptured__image.js
@@ -0,0 +1,7 @@
+var structcaptured__image =
+[
+    [ "camera_id", "structcaptured__image.html#a8b4fdeb140496169406945cf55f9a81e", null ],
+    [ "capture_delay", "structcaptured__image.html#ae76cf7d8185ddf5deb0eccb5f8f5c20e", null ],
+    [ "capture_times", "structcaptured__image.html#a9b9dc94e4039b8c8b8497078b9c7fdd4", null ],
+    [ "filename", "structcaptured__image.html#ac677dbb795432bbdea1d7ad5aa7167cf", null ]
+];
